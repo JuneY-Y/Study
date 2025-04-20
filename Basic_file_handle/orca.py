3b7c4e1b97c870dfd475bbafed3927cca0716c79
JuneY-Y <niyoumengma@gmail.com>
2025-04-20 16:10:04 +1000

    Auto-commit on 2025-04-20 16:10:04

--- Basic_file_handle/orca.py
+++ Basic_file_handle/orca.py
@@ -1,13 +1,16 @@
 import sys, re
-count=0
-for filename in sys.argv[1:]:
+count=0 #count 因为要累加，所以写在全局变量
+for filename in sys.argv[1:]: ##如果不这么写的话，会报错
+# 报错为:  with open(sys.argv[1:], "r") as f:
+#          ^^^^^^^^^^^^^^^^^^^^^^^
+# TypeError: expected str, bytes or os.PathLike object, not list
     with open(filename, "r") as f:
         for line in f: ## 
             words=line.strip().split() ##这里我需要提取第三行，动物名称。不会写了
             animal_name=" ".join(words[2:])
             if animal_name=="Orca":
-                count=int(words[1])
-                count=+ count
+                num=int(words[1]) ## 这里出问题了，一定要把它放到另一个容器里
+                count=count+num #两个数字进行累加
     
 print(f"{count} Orcas reported")
 
