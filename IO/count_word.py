43b49c35df41373ebecad7692ac475e8caf5cfba
JuneY-Y <niyoumengma@gmail.com>
2025-04-20 19:40:04 +1000

    Auto-commit on 2025-04-20 19:40:04

--- IO/count_word.py
+++ IO/count_word.py
@@ -1,28 +1,22 @@
 #!/usr/bin/env python3
 
+'''
+标准输入类型的题目
+'''
 
-# import sys, re
-# from collections import Counter
-
-# counter = Counter()
-# for line in sys.stdin:  ## 读取terminal的标准输入
-#     words=re.findall(r"[a-zA-Z]+", line) # re.findall(r"regex", line)
-#     words=[w.lower() for w in words] # for w in words , w应该是直接在这里就定义好了
-#     counter.update(words)
-# print(counter) 
-# '''
-# terminal: echo "Orca orca Whale whale WHALE" | python3 Counter.py
-# Counter({'whale': 3, 'orca': 2})
-# '''
-# print(f"{sum(counter.values())} words")
-
+#用这行命令进行测试 echo "Orca orca Whale ORCA"| python3 count_word.py orca
 import sys,re
 from collections import Counter
 counter= Counter()## 这个直接调用库就好
 for words in sys.stdin:
         w = re.findall(r"[a-zA-Z]+", words)
-        words = [word.lower() for word in w]
-        counter.update(words)
+        words = [word.lower() for word in w] ## 这里w是个list类型，✅
+        ##转为string，是要对每一个元素进行lower的处理⬆️
+        counter.update(words) #Counter()只用来构造 ✅
+        ## update是自动 每个类别的词进行更新的 ✅
+# ['orca', 'orca', 'whale', 'orca'] occurred Counter({'orca': 3, 'whale': 1}) times
 
-print(f"{word} occurred {counter} times")
+
+print(f"{target} occurred {counter[target]} times")
+# print(f"{words} occurred {counter} times")
     
