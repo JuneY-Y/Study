10c40d59343d718bb52571d25de16d5ef7fa1b11
JuneY-Y <niyoumengma@gmail.com>
2025-04-20 15:00:04 +1000

    Auto-commit on 2025-04-20 15:00:04

--- simple_sort.py
+++ simple_sort.py
@@ -3,8 +3,10 @@ import sys
 
 lines=sys.stdin.read().splitlines()
 lines=list(set(lines)) ##与sort|uniq效果相同
+lines.sort(key=str.lower)
+# words=[w.lower() for w in lines] ## 不区分大小写
 
-lines.sort() ## shell :sort
+# words.sort() ## shell :sort 按照字母排序
 
 for line in lines:
     print(line)
