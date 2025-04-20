a032e9f5ba6602e5c6da0fe2140ab12af76bf8e6
JuneY-Y <niyoumengma@gmail.com>
2025-04-20 18:30:04 +1000

    Auto-commit on 2025-04-20 18:30:04

--- dictionary/dictionary.py
+++ dictionary/dictionary.py
@@ -1,9 +1,11 @@
 import sys,re
+from collections import defaultdict
 '''
 	•	动物名称中，如果是 复数形式结尾的 s（如 Orcas），请去掉末尾的 s
 	•	如果是 以 's 结尾（如 Bryde's whale），保留 's 不做处理
 '''
-observations=[]
+# observations=[]
+observations=defaultdict(lambda:{'groups':0, 'total':0}) # 修改为全局变量✅
 pod=0
 for filename in sys.argv[1:]:
     with open(filename, "r") as f:
