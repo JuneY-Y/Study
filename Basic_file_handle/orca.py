b88e9fe3feae52ca183d39089a3eef802352d063
JuneY-Y <niyoumengma@gmail.com>
2025-04-20 15:40:03 +1000

    Auto-commit on 2025-04-20 15:40:03

--- Basic_file_handle/orca.py
+++ Basic_file_handle/orca.py
@@ -1,5 +1,8 @@
 import sys, re
 
-with open(sys.argv, "r") as f:
+with open(sys.argv[1:], "r") as f:
     for line in line:
+        words=line.strip().split() ##这里我需要
+        if words=="Orca":
+            catch
         
