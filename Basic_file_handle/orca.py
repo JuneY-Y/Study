eab5a51fc6f3cd0303be6380108d64d3a7c83ad2
JuneY-Y <niyoumengma@gmail.com>
2025-04-20 15:50:01 +1000

    Auto-commit on 2025-04-20 15:50:01

--- Basic_file_handle/orca.py
+++ Basic_file_handle/orca.py
@@ -2,7 +2,10 @@ import sys, re
 
 with open(sys.argv[1:], "r") as f:
     for line in line:
-        words=line.strip().split() ##这里我需要
-        if words=="Orca":
-            catch
+        words=line.strip().split() ##这里我需要提取第三行，动物名称。不会写了
+        animal_name=" ".join(words[2:])
+        if animal_name=="Orca":
+            count=int(words[1])
+            count=+ count
+            
         
