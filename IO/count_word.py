7d325744e63146597921d6c9124eb98b08278bc3
JuneY-Y <niyoumengma@gmail.com>
2025-04-20 19:20:04 +1000

    Auto-commit on 2025-04-20 19:20:04

---
+++ IO/count_word.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import sys,re
+from collections import Counter
+counter=[]
+for words in sys.stdin:
+    for words in sys.argv[1]:
+        w = re.findall(r"[a-zA-Z]+", line)
+        word=w.lower()
+        counter[]
+    
