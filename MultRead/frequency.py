f1c2473b6b6537547be76cb12754eb85a1b11774
JuneY-Y <niyoumengma@gmail.com>
2025-04-20 22:00:04 +1000

    Auto-commit on 2025-04-20 22:00:04

--- MultRead/frequency.py
+++ MultRead/frequency.py
@@ -1,6 +1,14 @@
 import sys, re
 import glob
+from collections import Counter
+'''
+<出现次数>/<总词数> = <频率> <歌手名>
+'''
 
 for file in glob.glob("/lyrics *.txt"): ## 需要带“”
+    with open(file,"r") as f:
+        for word in sys.stdin:
+            if 
+
 
     
