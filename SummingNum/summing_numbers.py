36b913f00d3eb4298721f5b2c09e04963f13cf42
JuneY-Y <niyoumengma@gmail.com>
2025-04-20 16:50:04 +1000

    Auto-commit on 2025-04-20 16:50:04

--- SummingNum/summing_numbers.py
+++ SummingNum/summing_numbers.py
@@ -1,7 +1,19 @@
 import sys, re
 '''
+🌟 list初始化
+🌟用extend进行insert element
+🌟sum() 只能接受int
+🌟map还是不是很懂
+	•	sys.argv[1]
+	•	with open(...) as f
+	•	re.findall(r"\d+", ...)
+	•	int() + sum(...)
 编写一个 Python 脚本，接收一个文本文件作为参数
 提取该文件中所有出现的数字（无论在哪里，只要是连续数字串），并输出这些数字的总和。
+with open(sys.argv[1]) as f:
+    text = f.read()  ##??????? f.read() 应该是另一种写法
+numbers = map(int, re.findall(r"\d+", text))
+print(sum(numbers))
 '''
 sums_up = []
 for filename in sys.argv[1:]:
