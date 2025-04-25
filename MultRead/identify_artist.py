fb9f18de670f82dae41265322121c13a5c20052b
JuneY-Y <niyoumengma@gmail.com>
2025-04-25 22:30:04 +1000

    Auto-commit on 2025-04-25 22:30:04

---
+++ MultRead/identify_artist.py
@@ -0,0 +1,9 @@
+#! usr/bin/env python3
+
+import collections, glob, math, os, re, sys
+from collections import Counter
+
+count = Counter()
+
+for filepath in sorted("./lyrics/ *.txt")
+# with open ("filename", r) as f:
