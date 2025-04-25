476a2021c459fb77f27335a68a75b11b93b72014
JuneY-Y <niyoumengma@gmail.com>
2025-04-26 08:50:03 +1000

    Auto-commit on 2025-04-26 08:50:03

--- MultRead/identify_artist.py
+++ MultRead/identify_artist.py
@@ -6,5 +6,21 @@ from collections import Counter, defaultdict ##defaultdict
 count = Counter()
 
 for filepath in sorted(glob.glob("./lyrics/ *.txt")):
+    with open(filepath, "r", encoding="utf-8") as f:
+        words=re.findall(r"[a-zA-Z]+", f.read().lower())
+    
+    artist=os.path.basename(filepath).replace(".txt", "").replace("_", "")
+    total=len(words)
+    counter=Counter(words)
+
+    default_log_prob=math.log(1//total)
+    ## 这里是构建艺术家，单个艺术家的log概率字典
+    log_probs=defaultdict(lambda: default_log_prob)
+    for word, count in couter.items():
+        log_probs[word]=math.log((count+1)/total)
+    artist_log_probs[artist]=log_probs
+
+for test_file in sys.argv[1:]:
+
     
 # with open ("filename", r) as f:
