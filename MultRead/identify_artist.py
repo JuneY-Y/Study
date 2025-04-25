5fb02009eab38ab70745750dbaa76a0d9b5c51ea
JuneY-Y <niyoumengma@gmail.com>
2025-04-26 08:40:02 +1000

    Auto-commit on 2025-04-26 08:40:02

--- MultRead/identify_artist.py
+++ MultRead/identify_artist.py
@@ -1,9 +1,10 @@
 #! usr/bin/env python3
 
 import collections, glob, math, os, re, sys
-from collections import Counter
+from collections import Counter, defaultdict ##defaultdict
 
 count = Counter()
 
-for filepath in sorted(glob.glob("./lyrics/ *.txt"))
+for filepath in sorted(glob.glob("./lyrics/ *.txt")):
+    
 # with open ("filename", r) as f:
