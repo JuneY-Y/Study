138ccc2ce8c79d4b935996f074ea8261c1703047
JuneY-Y <niyoumengma@gmail.com>
2025-04-30 15:30:01 +1000

    Auto-commit on 2025-04-30 15:30:01

--- sample/practice_q5.sh
+++ sample/practice_q5.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 file="$1"
-grep "Nobel Prize for physics" awards.psv | sort -t'|' -k2,2n | awk -F'|' '!seen[$2]++'
+
+n=$(grep "Nobel Prize for physics" awards.psv | sort -t'|' -k2,2n | awk -F'|' '!seen[$2]++' |head -n 1 | cut -d '|' -f2,2)
+
+m=$(grep "Nobel Prize for physics" awards.psv | sort -t'|' -k2,2n | awk -F'|' '!seen[$2]++' |tail -n 1 | cut -d '|' -f2,2)
+
+
+seq "$n" "$m"
