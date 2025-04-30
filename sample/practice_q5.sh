d9ddfcd97d34eab91083616249b947f2be8e3f53
JuneY-Y <niyoumengma@gmail.com>
2025-04-30 16:10:04 +1000

    Auto-commit on 2025-04-30 16:10:04

--- sample/practice_q5.sh
+++ sample/practice_q5.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
 # file="$1"
+regex="$1"
+file="$2"
 
+matching_lines=$(grep -E "^$regex$" "$file")
+
+if [ -z "$matching_lines" ]; then
+        echo "No awards matched"
+        exit 0
+fi 
+
+years_given=$(mktemp)
 n=$(grep "Nobel Prize for physics" awards.psv | sort -t'|' -k2,2n | awk -F'|' '!seen[$2]++' |head -n 1 | cut -d '|' -f2,2)
 
 m=$(grep "Nobel Prize for physics" awards.psv | sort -t'|' -k2,2n | awk -F'|' '!seen[$2]++' |tail -n 1 | cut -d '|' -f2,2)
