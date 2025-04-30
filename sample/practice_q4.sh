3277487142d3dcefadd5c4dcde8b7c03fc57c0ae
JuneY-Y <niyoumengma@gmail.com>
2025-04-30 14:10:03 +1000

    Auto-commit on 2025-04-30 14:10:03

--- sample/practice_q4.sh
+++ sample/practice_q4.sh
@@ -1,12 +1,16 @@
 #!/bin/dash
 
-file="$1"
+file="$1" ##
 
 n=$(sort -n "$file" | head -n 1)
-m=$(sort -n numbers_1.txt | tail -n 1)
+m=$(sort -n "$file" | tail -n 1) ## file这里让他不是硬编码 
 
 expected=$(mktemp)
 actual=$(mktemp)
 
 seq "$n" "$m" > "$expected"
 sort -n "$file" > "$actual"
+
+diff "$actual" "$expected" | grep '^>' |cut -d " " -f2 
+
+rm -f "$expected" "$actual"  ##
