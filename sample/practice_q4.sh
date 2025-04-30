f21481be5429c8b0aba2c20f6d64fcf56643a150
JuneY-Y <niyoumengma@gmail.com>
2025-04-30 14:00:03 +1000

    Auto-commit on 2025-04-30 14:00:03

--- sample/practice_q4.sh
+++ sample/practice_q4.sh
@@ -1,3 +1,12 @@
 #!/bin/dash
 
-sort -n | head -n1 |
+file="$1"
+
+n=$(sort -n "$file" | head -n 1)
+m=$(sort -n numbers_1.txt | tail -n 1)
+
+expected=$(mktemp)
+actual=$(mktemp)
+
+seq "$n" "$m" > "$expected"
+sort -n "$file" > "$actual"
