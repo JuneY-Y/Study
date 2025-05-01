f83aed408fa8286930ba29212995c3f7b64b8a92
JuneY-Y <niyoumengma@gmail.com>
2025-05-01 19:00:04 +1000

    Auto-commit on 2025-05-01 19:00:04

--- sample/practice_q9.py
+++ sample/practice_q9.py
@@ -1,1 +1,40 @@
 #!/usr/bin/env python3
+
+import sys
+
+n=int(sys.argv[1])
+filename=sys.argv[2]
+
+with open(filename, 'r') as f:
+    lines=f.readlines() ##read every line
+
+new_lines=[]
+
+for line in lines:
+    line = line.rstrip('\n')
+
+    while True:
+        if len(line) <= n:
+            new_lines.append(line)
+            break
+        
+        if ' ' not in line:
+            new_lines.append(line)
+            break
+        
+        front=line[:n]
+        ##🌟
+        if ' ' in front:
+            split_index=front.rfind(' ')
+            new_lines.append(line[:split_index])
+            line=line[split_index+1:]
+            continue
+        ##🌟
+        split_index=line.find(' ')
+        new_lines.append(line[:split_index])
+        line=line[split_index+1:]
+        continue
+
+with open(filename, 'w') as f:
+    for l in new_lines:
+        f.write(l + '\n')
