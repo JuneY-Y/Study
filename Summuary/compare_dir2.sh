840e6eef8078597b429470ec71b89d1a885099f7
JuneY-Y <niyoumengma@gmail.com>
2025-05-02 11:00:03 +1000

    Auto-commit on 2025-05-02 11:00:03

--- Summuary/compare_dir2.sh
+++ Summuary/compare_dir2.sh
@@ -1,8 +1,33 @@
+#!/bin/dash
+
+dir1="$1"
+dir2="$2"
+
 if [ $# -lt 2 ]; then
     echo "Usage: $0 <dir2> <dir2>"
     echo "must be two directory"
     exit 1
 fi
 
+##check arguments are empty 
+#-z = “zero-length”
 if [ -z "$dir1" ]; then
-    echo ""
+    echo "dir1 is an empty strings"
+    exit 1
+fi
+
+if [ -z "$dir2" ]; then
+    echo "dir2 is an empty strings"
+    exit 1
+fi
+
+## check directories
+if [ ! -d "$dir1" ]; then
+    echo "$dir1 is not a valid directory"
+    exit 1
+fi
+
+if [ ! -d "$dir2" ]; then
+    echo "$dir2 is not a valid directory"
+    exit 1
+fi
