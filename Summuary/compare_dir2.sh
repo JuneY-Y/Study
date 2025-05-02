01d1442d9931c1d9fb45a65e97291ba62d3d1204
JuneY-Y <niyoumengma@gmail.com>
2025-05-02 11:50:06 +1000

    Auto-commit on 2025-05-02 11:50:06

--- Summuary/compare_dir2.sh
+++ Summuary/compare_dir2.sh
@@ -31,3 +31,25 @@ if [ ! -d "$dir2" ]; then
     echo "$dir2 is not a valid directory"
     exit 1
 fi
+
+# check all and test 
+for file1 in "$dir1"/*  ##整个directory下的所有文件 "$dir1"/*
+do
+    file_name=$(basename "$file1")
+    echo "$filename"
+    file2="$dir2/$file_name"
+    echo "$file2"
+
+    if [ -e "$file2" ]; then ## 判断$dir2 是否在同一个目录里
+    '''
+    •	diff：比较两个文件内容
+	•	-i：忽略大小写（例如 A 和 a 视为一样）
+	•	-w：忽略所有空格（包括行末空格、缩进）
+	•	> /dev/null：不显示 diff 输出结果，只通过返回值判断是否相同
+	•	相同返回值为 0，即会进入 then 分支
+	•	不同返回值为非0
+    '''
+        if diff -i -w "$file1" "$file2" >/dev/null; then 
+            echo "$file_name" 
+        fi
+    fi
