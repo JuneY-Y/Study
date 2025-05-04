057237045edafb35c7cfb6a2246d9e8f5b405461
JuneY-Y <niyoumengma@gmail.com>
2025-05-05 09:50:04 +1000

    Auto-commit on 2025-05-05 09:50:04

--- sample/practice_q6.py
+++ sample/practice_q6.py
@@ -8,27 +8,15 @@ import sys
 输出：若不一致，指出哪一行不同；若一致，输出 "Mirrored"。
 '''
 
-# 抓取standin输入的内容为 $1 $2
-file1 = sys.argv[1]
-file2 = sys.argv[2]
+file1=sys.argv[1]
+file2=sys.argv[2]
 
+with open (file1, 'r')  as f1:
+    line1=[line.strip('\n') for line in f1]
 
-#打开文件
-with open (file1, 'r') as f1:
-    lines1 = [line.rstrip('\n') for line in f1]
+with open (file2, 'r')  as f1:
+    line2=[line.strip('\n') for line in f2]
 
-with open (file2, 'r') as f2:
-    lines2 = [line.rstrip('\n') for line in f2]
-
+#进行长度的匹配
 if len(lines1) != len(lines2):
     print(f"Not mirrored: different number of lines: {len(lines1)} versus {len(lines2)}")
-    sys.exit(0)
-
-#reversed_lines1=list(reversed(lines1))
-
-for i in range(len(lines1)):
-    if lines1[i] != lines2[-(i+1)] : # file2逆序比较的方法
-        print(f"Not mirrored: line {i+1} different")
-        sys.exit(0)
-
-print("Mirrored")
