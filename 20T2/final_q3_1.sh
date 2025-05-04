93dcefc8d35c7d98bb6cc059d4723e8f0769b036
JuneY-Y <niyoumengma@gmail.com>
2025-05-04 17:10:03 +1000

    Auto-commit on 2025-05-04 17:10:03

--- 20T2/final_q3_1.sh
+++ 20T2/final_q3_1.sh
@@ -1,4 +1,5 @@
-cut -d, -f1 enrollments.txt | sort | uniq -c | while read count. student_id ; do
-    if [ "student_id" -eq 1 ]
+cut -d, -f1 enrollments.txt | sort | uniq -c | while read count student_id ; do
+    if [ "count" -eq 1 ]; # 一个段落，会有 ";"
+    echo "$student_id"
     fi
 done 
