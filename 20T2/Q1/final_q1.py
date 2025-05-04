846b0450e7dd09066a4156a1ef14bf9ad9fb923f
JuneY-Y <niyoumengma@gmail.com>
2025-05-04 19:40:03 +1000

    Auto-commit on 2025-05-04 19:40:03

--- 20T2/Q1/final_q1.py
+++ 20T2/Q1/final_q1.py
@@ -7,3 +7,8 @@ for line in sys.stdin:
     zid=values[1]
     names=values[2].split(',')
     first_name=names[1].split()[0] ## ['Kevin', 'Augustus']
+    result.add((zid, first_name)) ## 添加元组映射
+
+result=[name for zid, name in result]
+for name in sorted(result):
+    print(name)
