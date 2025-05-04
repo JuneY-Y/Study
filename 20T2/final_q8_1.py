03a464a7ba3dbc715b9db0253935d76afdb7857e
JuneY-Y <niyoumengma@gmail.com>
2025-05-04 16:10:03 +1000

    Auto-commit on 2025-05-04 16:10:03

--- 20T2/final_q8_1.py
+++ 20T2/final_q8_1.py
@@ -2,3 +2,14 @@
 
 import sys
 from collections import Counter
+
+for line in sys.stdin:
+    words=line.strip().split()
+    result=[]
+    for word in words:
+        lower_word=word.lower() ##
+        count=Counter(lower_word)
+        freq=set(count.values())
+        if len(freq)==1:
+            result.append(word)
+    print(" ".join(result))
