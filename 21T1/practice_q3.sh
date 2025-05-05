0e0235edc869a1cae920b033e1eca321485848ea
JuneY-Y <niyoumengma@gmail.com>
2025-05-05 16:50:03 +1000

    Auto-commit on 2025-05-05 16:50:03

--- 21T1/practice_q3.sh
+++ 21T1/practice_q3.sh
@@ -3,7 +3,7 @@
 for file in *;do
     if [ -d "$file" ]; then
         #num=$(ls "$file" | wc -l)
-        count=$(find "$file" -mindepth 1 -maxdepth 1 | wc -l)
+        count=$(find "$file" -mindepth 1 -maxdepth 1 | wc -l) #直接寻找
         if [ "$count" -ge 2 ]; then
             echo "$file"
         fi
