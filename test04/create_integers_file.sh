a477821d62370fc53abcb4d85f6a7e60d5388230
JuneY-Y <niyoumengma@gmail.com>
2025-05-05 18:30:04 +1000

    Auto-commit on 2025-05-05 18:30:04

--- test04/create_integers_file.sh
+++ test04/create_integers_file.sh
@@ -4,13 +4,5 @@ end=$2
 filename=$3
 
 i=$start
-
-while [ $i -le $end ];do
-    if [ $i -eq $start ]; then
-        echo $i > $filename
-    else
-        echo $i >> $filename
-    fi
-    
-    i=$((i + 1))
-done
+while[ $i -le $end ];do
+    if 
