1db5d3a39329d7c44f6f24035502b6e488357ac5
JuneY-Y <niyoumengma@gmail.com>
2025-05-05 14:30:03 +1000

    Auto-commit on 2025-05-05 14:30:03

--- 21T1/practice_q4.sh
+++ 21T1/practice_q4.sh
@@ -1,16 +1,14 @@
 #!/bin/dash
 
-n=$1
-m=$2
+n=$(echo $1 | grep -Eo '[0-9]+')
+m=$(echo $2 | grep -Eo '[0-9]+')
 
-n=$(echo $n | grep -Eo '[0-9]+')
-m=$(echo $m | grep -Eo '[0-9]+')
+# echo $n
+# echo $m 
+current_num=$n  ##初始化
 
-echo $n
-echo $m
-
-while [ '$current_num' -le '$m' ]; do
-        result=$(echo $n | sed "s/n/$current_num/")
+while [ "$current_num" -le "$m" ]; do
+        result=$(echo $n | sed "s/$n/$current_num/")
         echo $result
         current_num=$((current_num+1))
 done
