39510378c3826ef0f04331a30ee9505e89b2f41d
JuneY-Y <niyoumengma@gmail.com>
2025-05-08 17:00:01 +1000

    Auto-commit on 2025-05-08 17:00:01

--- test04/htm2html.sh
+++ test04/htm2html.sh
@@ -1,7 +1,6 @@
 #!/bin/dash
 
-## 修改htm为html
-for htm_file in *.htm;do
+for htm_file in *.htm; do
     if [ "$htm_file" = "*.htm" ]; then
         exit 0
     fi
@@ -13,28 +12,18 @@ for htm_file in *.htm;do
     mv "$htm_file" "$html_filename"
 done
 
-#!/bin/dash
 
-for htm_file in *.htm; do
-    if [ "$htm_file" = "*.htm" ]; then
-        exit 0
-    fi
-    html_filename="${htm_file%.*}.html"
-    if [ -f "$html_filename" ]; then
-    
+# #!/bin/dash
 
+# ## 修改htm为html
 # for htm_file in *.htm;do
-
 #     if [ "$htm_file" = "*.htm" ]; then
 #         exit 0
 #     fi
-    
 #     html_filename="${htm_file%.*}.html"
-    
 #     if [ -f "$html_filename" ]; then
 #         echo "$html_filename exists"
 #         exit 1
 #     fi
-    
 #     mv "$htm_file" "$html_filename"
 # done
