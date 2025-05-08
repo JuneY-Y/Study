8dfd25d254e40a61730389675c33a4e661f77bd2
JuneY-Y <niyoumengma@gmail.com>
2025-05-08 17:40:03 +1000

    Auto-commit on 2025-05-08 17:40:03

--- DataStru/DASH/diff.sh
+++ DataStru/DASH/diff.sh
@@ -1,3 +1,15 @@
 #!/bin/dash
 
+# 检查文件是否存在
 if [ ! -f "diff_1.txt" ] || [ ! -f "diff_2.txt" ]; then
+    echo "no file exit"
+    exit 1
+fi
+
+# 使用diff -q检查文件是否相同
+if diff -q diff_1.txt diff_2.txt > /dev/null; then
+    echo "the same file"
+else
+    echo "not similar"
+fi
+
