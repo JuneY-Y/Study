c5d7c674c4942ab7cc0c716307a639d88d731cac
JuneY-Y <niyoumengma@gmail.com>
2025-05-09 16:20:04 +1000

    Auto-commit on 2025-05-09 16:20:04

--- co_sh_py/selftest/script.py
+++ co_sh_py/selftest/script.py
@@ -7,6 +7,11 @@ python3 script.py           # 报错：Usage: script.py <course code>
 exam point
 	•	assert 条件, 错误信息：
 	•	如果条件为 False，程序会抛出 AssertionError，并输出错误信息
+
+    [A-Z]{4}([0-9]{4})? 表示如下：
+    •	[A-Z]{4}：匹配 4 个大写字母。
+	•	([0-9]{4})?：表示 可选的 4 个数字。
+
 '''
 import sys
 import re
@@ -16,6 +21,11 @@ if len(sys.argv) != 2:
     sys.exit(1)
 course_prefix=sys.argv[1]
 
+if not re.fullmatch(r"[A-Z]{4}([0-9]{4})", course_prefix):
+    print(f"Invalid course code: {course_prefix}")
+    sys.exit(1)
+
+print(f"Valid course code: {course_prefix}")
 
 
 # course_prefix= sys.argv[1]
