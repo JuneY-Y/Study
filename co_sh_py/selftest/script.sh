8feb08396882664f4ca8141c5387822e323b3d20
JuneY-Y <niyoumengma@gmail.com>
2025-05-09 15:20:04 +1000

    Auto-commit on 2025-05-09 15:20:04

--- co_sh_py/selftest/script.sh
+++ co_sh_py/selftest/script.sh
@@ -2,10 +2,18 @@
 
 course_code=$1
 
-if [ ! -f "course_code" ]; then
-    echo "Usage: $0 <course code>"
+if [ -f "course_code" ]; then :
+    echo "doesn't have: <$course_code>"
 fi
 
-if [ ! -f "course_code" ]; then
-    echo "Invalid course code: <$1>"
+
+if [ "$#" -lt 1 ]; then  ## 🌟
+    echo "Invalid course code: $0 <argument>"
+    exit 1
+fi
+
+if echo "$course_code" | grep -E "[A-Z]{4}[0-9]{4}" > /dev/null; then :
+else
+    echo "Usage: $0 <course code>"
+    exit 1
 fi
