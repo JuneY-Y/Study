eccae67eb45f0971b7f403a1394600537c6afb14
JuneY-Y <niyoumengma@gmail.com>
2025-05-09 15:50:02 +1000

    Auto-commit on 2025-05-09 15:50:02

---
+++ co_sh_py/selftest/script.py
@@ -0,0 +1,16 @@
+#!usr/bin/env python3
+'''
+python3 script.py COMP1511  # 通过
+python3 script.py COMP      # 报错：Invalid course code: COMP
+python3 script.py           # 报错：Usage: script.py <course code>
+
+exam point
+	•	assert 条件, 错误信息：
+	•	如果条件为 False，程序会抛出 AssertionError，并输出错误信息
+'''
+import sys
+import re
+
+course_prefix= sys.argv[1]
+assert len(sys.argv)==1, f"Usage: {sys.argv[0]} <course prefix>"
+assert re.fullmatch(r"[A-Z]{4}", course_prefix), f"Invalid course code: {}"
