347aeac47ef819f4c6ea4f478358b8f0fd31e5b7
JuneY-Y <niyoumengma@gmail.com>
2025-05-12 07:50:04 +1000

    Auto-commit on 2025-05-12 07:50:04

--- co_sh_py/selftest/sort1.py
+++ co_sh_py/selftest/sort1.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python3
 
-with open "enrollments.txt" as f:
+courses=set()
+with open ("enrollments.txt") as f:
+    for line in f:
+        course=line.strip().split('|')[0] ### split是需要('|') 这样进行的，而且需[0]
+        print(course)
+        courses.add(course)
+
+print(sorted(course))
+        
+    
