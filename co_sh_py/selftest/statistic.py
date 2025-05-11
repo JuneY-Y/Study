097e68990e89374a3e518415c0342525e580537f
JuneY-Y <niyoumengma@gmail.com>
2025-05-11 11:10:03 +1000

    Auto-commit on 2025-05-11 11:10:03

--- co_sh_py/selftest/statistic.py
+++ co_sh_py/selftest/statistic.py
@@ -3,6 +3,22 @@
 import sys
 import re
 
+course_code=sys.argv[1]
+
 if len(sys.argv) != 2:
     print(f"Usage: {sys.argv[0]} <course code>")
     sys,exit(1)
+
+if not re.fullmatch(r"[A-Z]{4}[0-9]{4}", course_code):
+    print(f"Invalid course code: {course_code}")
+    sys.exit(1)
+
+male_count=0
+female_count=0
+
+with open("enrollments.txt") as file:
+    for line in file:
+        if line.startswith(course_code): ##why use startswith
+            gender=line.strip().split('|')[4]
+            if 
+
