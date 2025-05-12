57de6d444345a985fe65536541db0baafd3903bd
JuneY-Y <niyoumengma@gmail.com>
2025-05-12 19:50:03 +1000

    Auto-commit on 2025-05-12 19:50:03

--- co_sh_py/selftest/exam/practice_q3.py
+++ co_sh_py/selftest/exam/practice_q3.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python3
 
 '''
-
 grep -iE ".*\|M$" | cut -d'|' -f3,3|cut -d ',' -f1|sort |uniq
-
 '''
 import sys
 import re
 
+result=set()
 with open ("enrollments.txt") as f:
+    for line in f:
+        line=line.strip()
+        if re.search(r'\|M$', line,re.IGNORECASE):
+            print(line)
+            name=line.split('|')[2]
+            print(name)
+            surname=name.split(',')[0]
+            result.add(surname)
+
+for results in sorted(result):
+    print(results)
