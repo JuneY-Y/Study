bf9572800ccedc4b7a40fd2f51f0a6b793d82bc1
JuneY-Y <niyoumengma@gmail.com>
2025-05-14 08:30:03 +1000

    Auto-commit on 2025-05-14 08:30:03

--- shell/missingnumber1.sh
+++ shell/missingnumber1.sh
@@ -1,18 +1,18 @@
 #!/bin/dash
 
+## 先不用flag 用python里的for n not in m
 ## 用flag判断 missing number
 ## 读取文件
 
 # bolloon flag="flase"
 filename=$1
 
-numbers=$(cat $filename| sort|uniq|sed 's/ /\\n/g')
+numbers=$(cat "$filename"| sort -n)
 # echo $numbers
-for num in $numbers; do :
-    # echo $num
-done
+# for num in $numbers; do :
+#     # echo $num
+# done
 
-min=$(echo $numbers|head -n1)
-max=$(echo $numbers|tail -n1)
-echo $min
-# echo $max
+min=$(echo "$numbers"|head -n1) # "$numbers"
+max=$(echo "$numbers"|tail -n1)
+# echo $min,$max
