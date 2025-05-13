d52d75536776489383e38e3ef8f4e9774f4d95b9
JuneY-Y <niyoumengma@gmail.com>
2025-05-14 09:10:04 +1000

    Auto-commit on 2025-05-14 09:10:04

--- shell/final_q4.sh
+++ shell/final_q4.sh
@@ -2,5 +2,17 @@
 
 filename=$1
 
-numbers=$(cat "$filename"|sort -n)
-echo $numbers
+numbers=$(cat "$filename" | sort -n)
+# echo $numbers
+min=$(echo "$numbers"|head -n1)
+max=$(echo "$numbers"|tail -n1)
+
+count_num=$min
+
+while [ $count_num -le $max ]; do
+
+    if ! echo “$numbers”| grep -qE “$count_num”;then ##条件判断语句在dash里面没有()
+        echo $count_num
+    fi
+    count_num=$(( count_num+1 ))
+done
