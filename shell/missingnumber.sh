d18ea2468696607d3055c5cca20185350271665e
JuneY-Y <niyoumengma@gmail.com>
2025-05-13 17:20:04 +1000

    Auto-commit on 2025-05-13 17:20:04

--- shell/missingnumber.sh
+++ shell/missingnumber.sh
@@ -1,6 +1,6 @@
 #!/bin/dash
 
-filename= $1
+filename=$1
 
 n=$(cat $filename|head -n1)
 echo "$n"
@@ -8,3 +8,15 @@ m=$(cat $filename|tail -n1)
 echo "$m"
 expect_num=$(((m-n+1)*(m-n)/2))
 echo "$expect_num"
+total=0
+
+for num in $n; do  ##这里直接for loop了$n中文档的数字个数
+    echo num
+    echo "$num"
+    total=$((total+num))
+done
+
+missingnumber=$((expect_num-total))
+if [ $missingnumber -ne 0 ]; then
+    echo $missingnumber
+fi
