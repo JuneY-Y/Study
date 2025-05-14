b30e7fe2df49de43b1687d9d709a72d838a0a8dd
JuneY-Y <niyoumengma@gmail.com>
2025-05-14 16:40:04 +1000

    Auto-commit on 2025-05-14 16:40:04

--- shell/practice_q5.sh
+++ shell/practice_q5.sh
@@ -1,26 +1,27 @@
 #!/bin/dash
 ## Find Years an Award Was Not Given
+# -ne是数字比较
 
 award=$1
 filename=$2
 real_award=$(cut -d '|' -f1 $filename)
-if [ $award -ne $real_awrd ];then
+
+if ! echo "$real_award"|grep -qE "^$award$";then
     echo "practice_q5.sh: no awards match '"$award"'"
 fi
 
 # echo "$real_award"
 for awards in $real_award;do
-    years=$(grep -E "$awards" "$filename"| cut -d '|' -f2,2|sort -n|uniq)
+    years=$(grep -E "^$awards$" "$filename"| cut -d '|' -f2,2|sort -n|uniq)
     # echo "$years"
     start=$(echo "$years"|head -n1)
     end=$(echo "$years"|tail -n1)
     # echo "$start"
     start_count=$start
     while [ $start_count -le $end ]; do
-        if ! echo $start_count| grep -Eq $years ;then
+        if ! echo "$years"| grep -qE "^$start_count$" ;then
             echo "$start_count"
         fi
+        start_count=$((start_count+1))
     done
-    start_count=$((start_count+1))
-
 done
