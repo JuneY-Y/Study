2a0bcab583af32c1b35367fae749c570dd8af254
JuneY-Y <niyoumengma@gmail.com>
2025-05-14 16:20:03 +1000

    Auto-commit on 2025-05-14 16:20:03

--- shell/practice_q5.sh
+++ shell/practice_q5.sh
@@ -3,10 +3,18 @@
 
 award=$1
 filename=$2
-
-real_award=$(cut -d '|' -f1 $filename)
+# real_award=$(cut -d '|' -f1 $filename)
 # echo "$real_award"
 for awards in $award;do
-    years=$(grep -E "$awards" "$filename"| )
+    years=$(grep -E "$awards" "$filename"| cut -d '|' -f2,2|sort -n|uniq)
+    # echo "$years"
+    start=$(echo "$years"|head -n1)
+    end=$(echo "$years"|tail -n1)
+    # echo "$start"
+    start_count=$start
+    while [ $start_count -le $end ]; do
+        if ! echo 
+    done
+    start_count=$((start_count+1))
 
 done
