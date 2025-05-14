bb3979bb8dbf9a7c54ec4a68f2f657280a9519e3
JuneY-Y <niyoumengma@gmail.com>
2025-05-14 12:10:03 +1000

    Auto-commit on 2025-05-14 12:10:03

--- shell/practice_q2.sh
+++ shell/practice_q2.sh
@@ -6,6 +6,10 @@ echo "$count_man"
 count_woman=$(grep -E "\|F$" "$filename")
 echo "$count_woman"
 # while read -r "$filename"; do
-
+for line1 in "$count_man"; do
+    for line2 in "$count_woman"; do
+        if grep -qE "$line1"
+    done
+done
 
 # done
