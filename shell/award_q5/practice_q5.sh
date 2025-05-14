0e0bbc3fb048a0e59ccf6780e102fb9feac2aebb
JuneY-Y <niyoumengma@gmail.com>
2025-05-15 08:20:03 +1000

    Auto-commit on 2025-05-15 08:20:03

--- shell/award_q5/practice_q5.sh
+++ shell/award_q5/practice_q5.sh
@@ -3,4 +3,6 @@
 award_name=$1
 filename=$2
 
-years= $(grep -E "$award_name" "$filename"|cut -d '|' -f2,2 )
+# years=$(grep -E "^$1\|" "$2" | sort -t'|' -k2 | cut -d'|' -f2)
+years= $(grep -E "^$award_name\|" "$filename"|cut -d '|' -f2|sort |uniq )
+echo "$years"
