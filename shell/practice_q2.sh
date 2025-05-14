b4fb954dde955b97463563193a2098d78bd1c6b2
JuneY-Y <niyoumengma@gmail.com>
2025-05-14 12:00:04 +1000

    Auto-commit on 2025-05-14 12:00:04

--- shell/practice_q2.sh
+++ shell/practice_q2.sh
@@ -1,8 +1,11 @@
 #!/bin/dash
- filename=$1
- echo $filename
+filename=$1
+echo $filename
+count_man=$(grep -E "\|M$" "$filename")
+echo "$count_man"
+count_woman=$(grep -E "\|F$" "$filename")
+echo "$count_woman"
+# while read -r "$filename"; do
 
- while read -r "$filename"; do
 
-
- done
+# done
