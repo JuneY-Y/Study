ed5e7817e591c0255bfaed71f025b29ebd9488ed
JuneY-Y <niyoumengma@gmail.com>
2025-05-14 13:20:01 +1000

    Auto-commit on 2025-05-14 13:20:01

--- shell/practice_q2.sh
+++ shell/practice_q2.sh
@@ -1,22 +1,28 @@
 #!/bin/dash
-filename=$1
-echo $filename
-count_man=$(grep -E "\|M$" "$filename")
-echo "$count_man"
-count_woman=$(grep -E "\|F$" "$filename")
-echo "$count_woman"
-# while read -r "$filename"; do
-for line1 in "$count_man"; do
-    for line2 in "$count_woman"; do
-        man=$(grep -E "^[A-Za-z]{4}[0-9]{4}" "$line1"|wc -l)
-        woman=$(grep -E "^[A-Za-z]{4}[0-9]{4}" "$line2"|wc -l)
-        if [ "$man" -eq "$woman" ];then
-            echo "result:"
-            result=$(grep -oE "^[A-Za-z]{4}[0-9]{4}" '$line1'|cut -d '|' -f1)
-            # echo $(grep -Eo "^[A-Za-z]{4}[0-9]{4}" "$line1")
-            echo $result
-        fi
-    done
-done
 
+filename=$1
+courses=$(cut -d'|' -f1 "$filename"|sort|uniq)
+echo "$courses"
+
+# #!/bin/dash
+# filename=$1
+# echo $filename
+# count_man=$(grep -E "\|M$" "$filename")
+# echo "$count_man"
+# count_woman=$(grep -E "\|F$" "$filename")
+# echo "$count_woman"
+# # while read -r "$filename"; do
+# for line1 in "$count_man"; do
+#     for line2 in "$count_woman"; do
+#         man=$(grep -E "^[A-Za-z]{4}[0-9]{4}" "$line1"|wc -l)
+#         woman=$(grep -E "^[A-Za-z]{4}[0-9]{4}" "$line2"|wc -l)
+#         if [ "$man" -eq "$woman" ];then
+#             echo "result:"
+#             result=$(grep -oE "^[A-Za-z]{4}[0-9]{4}" '$line1'|cut -d '|' -f1)
+#             # echo $(grep -Eo "^[A-Za-z]{4}[0-9]{4}" "$line1")
+#             echo $result
+#         fi
+#     done
 # done
+
+# # done
