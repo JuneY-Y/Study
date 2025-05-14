0c054e99c63a47214fcc4b27f61da66117202621
JuneY-Y <niyoumengma@gmail.com>
2025-05-14 11:30:02 +1000

    Auto-commit on 2025-05-14 11:30:02

--- shell/practice_q3.sh
+++ shell/practice_q3.sh
@@ -1,5 +1,16 @@
 #!/bin/dash
 
-while [ -f "#$@" ]; do
+# while [ -f "#$@" ]; do
+# done < dictionary
 
-done < dictionary
+for item in *; do
+    if [ -d "$item" ]; then
+        count=$(ls -A "$item"|wc -l)
+        if [ "$count" -ge 2 ]; then
+            echo "$item"
+        fi
+    fi
+    # if [ "$count" -ge 2 ]; then
+    #     echo "$item"
+    # fi
+done
