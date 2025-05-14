21ae83c7bad6f85be5adad6f25d47bd1692e2a3e
JuneY-Y <niyoumengma@gmail.com>
2025-05-15 08:30:02 +1000

    Auto-commit on 2025-05-15 08:30:01

--- shell/award_q5/practice_q5.sh
+++ shell/award_q5/practice_q5.sh
@@ -4,5 +4,16 @@ award_name=$1
 filename=$2
 
 # years=$(grep -E "^$1\|" "$2" | sort -t'|' -k2 | cut -d'|' -f2)
-years= $(grep -E "^$award_name\|" "$filename"|cut -d '|' -f2|sort |uniq )
+years=$(grep -E "^$award_name\|" "$filename"|cut -d '|' -f2|sort |uniq ) ##需要检查synatx
 echo "$years"
+if [ -z "$years" ];then ## 不止1年的year，所以需要“”
+    echo "No award matching "$years""
+    exit 1
+fi
+
+start_year=$(echo "$years"|head -n1)
+echo "$start_year"
+end_year=$(echo "$years"|tail -n1)
+echo "$end_year"
+count=$start_year
+while [ $count -le $end_year ]
