03badacae0acba8896040deca94818f2c67c118c
JuneY-Y <niyoumengma@gmail.com>
2025-05-14 10:10:03 +1000

    Auto-commit on 2025-05-14 10:10:03

--- shell/practice_q4.sh
+++ shell/practice_q4.sh
@@ -1,15 +1,21 @@
 #!/bin/dash
-
+# Sequence Generator by Replacing Integer in a String
 string1=$1
 string2=$2
 # echo "$string"
 n=$(echo "$string1"| grep -Eo "[0-9]+")
-echo "$n"
+# echo "$n"
 m=$(echo "$string2"| grep -Eo "[0-9]+")
-echo "$m"
+# echo "$m"
+star_char=$(echo "$string1"|grep -Eo "^[A-Za-z]+") ##正则的时候，直接匹配到行首和行尾符号即可
+end_char=$(echo "$string2"|grep -Eo "[A-Za-z]+$")
+# echo "$star_char"
+# echo "$end_char"
 count_num=$n
 while [ $count_num -le $m ]; do
-    echo "$count_num"
+    # echo "$count_num"
+    echo "$star_char""$count_num""$end_char"
+    
     count_num=$(( count_num+1 ))
     # echo $count_num
 done
