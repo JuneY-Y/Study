b8307e4efb98fbfa494baf88d0b5986959650331
JuneY-Y <niyoumengma@gmail.com>
2025-05-17 16:50:09 +1000

    Auto-commit on 2025-05-17 16:50:09

--- sample1/practice_q7.sh
+++ sample1/practice_q7.sh
@@ -5,26 +5,27 @@
 #如果没有if fi那么进行添加（题意是不需要添加的） extension就是开头那个破东西
 #识别的话 regex=$(grep -xE "^[#!]/.*")
 #添加成功以后推出
+## 
 
 for pathname in "$@"; do  ##想不明白，为什么要读取pathname
-    case "$file" in
-        *.*)
-            echo "# $file already has an extension"
-            continue;;
-    esac
-
-    if ! head -n 1 "$file" | grep -q '^#!';then
-        echo "# $file does not have a #! line"
-        continue
-    fi
-    first_line=$(head -n 1 "$file")
-
-    # case "$(head -1 "$pathname")" in
-    #     *perl*) extension="pl";;
-    #     *python*) extension="py";;
-    #     *sh*) extension="sh";;
-    #     *) extension="";;
+    # case "$file" in
+    #     *.*)
+    #         echo "# $file already has an extension"
+    #         continue;;
     # esac
+
+    # if ! head -n 1 "$file" | grep -q '^#!';then
+    #     echo "# $file does not have a #! line"
+    #     continue
+    # fi
+    # first_line=$(head -n 1 "$file")
+
+    case "$(head -1 "$pathname")" in
+        *perl*) extension="pl";;
+        *python*) extension="py";;
+        *sh*) extension="sh";;
+        *) extension="";;
+    esac
 
 
     new_pathname="$pathname.$extension"
