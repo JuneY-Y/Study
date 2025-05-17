1993b9498d9daf5e1b86c14a3788e3db75a4a235
JuneY-Y <niyoumengma@gmail.com>
2025-05-17 15:20:02 +1000

    Auto-commit on 2025-05-17 15:20:02

--- sample1/practice_q7.sh
+++ sample1/practice_q7.sh
@@ -1,5 +1,5 @@
 #!/bin/dash
-## 直接with open file,但是shell里面应该是 read -r
+## 直接with open file,但是shell里面应该是 read -r 为什么不用这个？
 # 这里面应该是while read -r; do done?
 #读取第一行，咋读啊
 #如果没有if fi那么进行添加（题意是不需要添加的） extension就是开头那个破东西
@@ -7,25 +7,32 @@
 #添加成功以后推出
 
 for pathname in "$@"; do  ##想不明白，为什么要读取pathname
-    case "$(head -1 "$pathname")" in
-        *perl*) extension="pl";;
-        *python*) extension="py";;
-        *sh*) extension="sh";;
-        *) extension="";;
+    case "$file" in
+        *.*)
+            echo "# $file already has an extension"
+            continue;;
     esac
+
+    # case "$(head -1 "$pathname")" in
+    #     *perl*) extension="pl";;
+    #     *python*) extension="py";;
+    #     *sh*) extension="sh";;
+    #     *) extension="";;
+    # esac
+
 
     new_pathname="$pathname.$extension"
 
     if echo "$pathname" | grep -Eq '\.[^/]+$'; then
-        echo "# $pathname already has an extension"
+        echo "# $pathname already has an extension" ##识别已经有了这个扩展名，所以跳过
     elif [ "$(head -c 2 "$pathname")" != '#!' ];then
-        echo "# $pathname does not have a #! line"
+        echo "# $pathname does not have a #! line" ##
     elif [ -z "$extension" ];then
         echo "# $pathname no extension for #! line"
     elif [ -e "$new_pathname" ]; then
         echo "# $new_pathname already exists"
     else
-        echo "mv $ppathname $new_pathname"
+        echo "mv $pathname $new_pathname"
     fi
 
 done
