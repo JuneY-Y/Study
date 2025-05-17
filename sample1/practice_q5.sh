11b642d4cb94149498f9abbc73385f4f53c5aca0
JuneY-Y <niyoumengma@gmail.com>
2025-05-17 12:50:04 +1000

    Auto-commit on 2025-05-17 12:50:04

--- sample1/practice_q5.sh
+++ sample1/practice_q5.sh
@@ -3,4 +3,16 @@
 award=$1
 file=$2
 
-years$()
+years=$(grep -E "$award" "$file"|cut -d '|' -f2|sort -n|uniq)
+echo "$years"
+head=$(echo "$years"|head -n1)
+end=$(echo "$years"|tail -n1)
+# echo "$head" "$end"
+if [ -e "$years" ];then
+    echo no matching "$award">&2
+    exit 1
+fi
+count_year=$head
+while [ $head -le $end ];do
+    
+done
