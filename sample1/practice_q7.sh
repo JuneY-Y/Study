139d6c7096bce6dc4f92dc3d110d1467ceeda003
JuneY-Y <niyoumengma@gmail.com>
2025-05-21 20:40:03 +1000

    Auto-commit on 2025-05-21 20:40:03

--- sample1/practice_q7.sh
+++ sample1/practice_q7.sh
@@ -4,9 +4,9 @@ newfilename=$("$filename.$extension")
 for filename in "$@";do
     if echo "$filename" |grep -E "\.[A-Za-z]+";then
         echo "# filename already has an extension"
-    elif [ "$(head -n 1 "$filename")" != "#!" ]; then   ## 这里是个什么写法呢？
+    elif [ "$(head -1 "$filename")" != "#!" ]; then   ## 这里是个什么写法呢？
         echo "# filename does not have a #! line"
-    elif [ "$(head -n 1 "$filename")" != "python|sh|perl" ];then
+    elif [ "$(head -1 "$filename")" != "python|sh|perl" ];then
         echo "# filename no extension for #! line"
     elif echo "$newfilename" |grep -E "\.[A-Za-z]+";then
         echo "# filename.<ext> already exists"
