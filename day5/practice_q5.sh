b48322e470fd3c904d99cb8d19f10de8cdc2dd1d
JuneY-Y <niyoumengma@gmail.com>
2025-05-23 20:40:01 +1000

    Auto-commit on 2025-05-23 20:40:01

--- day5/practice_q5.sh
+++ day5/practice_q5.sh
@@ -3,5 +3,21 @@
 award=$1
 file=$2
 
-year=$(grep -E "$1" $2|cut -d '|' -f2|sort|uniq)
-echo "$year"
+year=$(grep -E "^$1" $2|cut -d '|' -f2|sort -n|uniq)
+# echo "$year"
+if [ -z "$year" ];then
+    echo "No award matching '$1'"
+    exit 0
+fi
+
+n=$(echo "$year"|head -n1)
+m=$(echo "$year"|tail -n1)
+start="$n"
+while [ $start -le $m ];do
+    if ! echo "$year"|grep -Eq "$start"; then
+        echo "$start"
+    fi
+    start=$((start+1))
+done
+# echo "$n"
+# echo "$m"
