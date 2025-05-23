f1b6d2e1f246bf5d0b4b0448427a0a1bc3c9c47a
JuneY-Y <niyoumengma@gmail.com>
2025-05-23 21:30:05 +1000

    Auto-commit on 2025-05-23 21:30:04

--- day5/practice_q6.py
+++ day5/practice_q6.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
+#这道题需要重新琢磨一下，并且看看考点是什么
+#test8 是个坑，题目中说只输出一行不对的，因此输出一行即可
+#还是需要读题的
 import sys
 
 file1=sys.argv[1]
 file2=sys.argv[2]
 
 with open(file1) as f1, open(file2) as f2:
-    lines1=[line.rstrip('\n') for line in f1]
+    lines1=[line.rstrip('\n') for line in f1] ##每个处理的都需要\n
     lines2=[line.rstrip('\n') for line in f2]
 
 if len(lines1)!=len(lines2):
@@ -16,4 +19,5 @@ else:
     for i in range(len(lines1)):
         if lines1[i] != lines2[-1-i]:
             print(f"Not mirrored: line {i+1} different")
+            sys.exit(0)
             
