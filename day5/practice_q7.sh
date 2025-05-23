9dfc6c0c158c38359121828e6e00e07ec848e49e
JuneY-Y <niyoumengma@gmail.com>
2025-05-23 23:40:01 +1000

    Auto-commit on 2025-05-23 23:40:01

--- day5/practice_q7.sh
+++ day5/practice_q7.sh
@@ -1,12 +1,22 @@
 #!/bin/dash
 
 for file in "$@"; do
-    if echo "$file"| grep -Eq "\."; then
+    if echo "$file"| grep -Eq "\.[A-Za-z]+"; then
         echo "# $file already has an extension"
         continue
     fi
 
-    if head -n1 "$file"|grep -Eq "^#!"; then
+    if ! head -n 1 "$file"|grep -Eq "^#!"; then ## 注意 ！有还是没有
         echo "# $file does not have a #! line"
+        continue
+    fi
+
+    ##If the #! line does not contain any of the strings, perl, python or sh,
+    #所以需要先获取shebang
+    first_line=$(head -n 1 "$file")
+    if echo "$first_line"| grep -Eq "perl|sh|perl";then
+        echo "# $file does not have a #! line"
+        continue
+    fi
 
 done
