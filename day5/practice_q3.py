a061b043ed2c86f6e280bb9cada3931a2bf476bf
JuneY-Y <niyoumengma@gmail.com>
2025-05-23 19:10:04 +1000

    Auto-commit on 2025-05-23 19:10:04

--- day5/practice_q3.py
+++ day5/practice_q3.py
@@ -1,35 +1,38 @@
 #!/usr/bin/env python3
 import sys
 ## 用什么写什么。先把名字都放进surname set里面
+#set不能直接打印
+#🌟 错误点，处理line的时候，首先需要strip()
 # grep -E '\|M$'|cut -d '|' -f3|cut -d "," -f1|tr -d ' '|sort|uniq
 
 
 surnames=set()
 
-for line in sys.stdin:
+for line in sys.stdin: ##默认有\n
     ## 这里需要进行处理
-    # line=line.strip()
-    if line.endswith('\|M'):
+    line=line.strip()
+    if line.endswith('|M'):
         part=line.strip().split('|')
         name=part[2]
-        surname=name.split(',')[0]
-        surnames.add(surname)
-
-print(surnames)
-
-#!/usr/bin/env python3
-import sys
-
-surnames = set()
-
-for line in sys.stdin:
-    line=line.strip()
-
-    if line.endswith('|M'):
-        parts=line.split('|')
-        name=parts[2]
         surname=name.split(',')[0].strip()
         surnames.add(surname)
 
+
 for s in sorted(surnames):
     print(s)
+# #!/usr/bin/env python3
+# import sys
+
+# surnames = set()
+
+# for line in sys.stdin:
+#     line=line.strip()
+
+#     if line.endswith('|M'):
+#         parts=line.split('|')
+#         name=parts[2]
+#         surname=name.split(',')[0].strip()
+#         surnames.add(surname)
+
+# for s in sorted(surnames):
+#     print(s)
