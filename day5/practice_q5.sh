d1710b8f0792311af568ea242bcae7c5acdd6869
JuneY-Y <niyoumengma@gmail.com>
2025-05-23 20:50:04 +1000

    Auto-commit on 2025-05-23 20:50:04

--- day5/practice_q5.sh
+++ day5/practice_q5.sh
@@ -1,9 +1,9 @@
 #!/bin/dash
-
+# [^ ]+ [^| ]+  先查看正则的问题再说
 award=$1
 file=$2
 
-year=$(grep -E "^$1" $2|cut -d '|' -f2|sort -n|uniq)
+year=$(grep -E "^$award\|" "$file"|cut -d '|' -f2|sort -n|uniq)
 # echo "$year"
 if [ -z "$year" ];then
     echo "No award matching '$1'"
@@ -13,7 +13,7 @@ fi
 n=$(echo "$year"|head -n1)
 m=$(echo "$year"|tail -n1)
 start="$n"
-while [ $start -le $m ];do
+while [ "$start" -le "$m" ];do
     if ! echo "$year"|grep -Eq "$start"; then
         echo "$start"
     fi
