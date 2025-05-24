752a35a722dbe0de4dcbdc76c0cd63861264bbef
JuneY-Y <niyoumengma@gmail.com>
2025-05-24 10:30:04 +1000

    Auto-commit on 2025-05-24 10:30:04

---
+++ day5/practice_q8.sh
@@ -0,0 +1,9 @@
+#!/bin/dash
+
+# 这道题用 flag 变量 printed 来判断是否有 ln -s 输出。
+# 我们使用双重 for file1 in "$@" + for file2 in "$@" 来实现所有两两文件配对比较，
+# 每次只比较不相同的文件名。为了避免重复处理同一文件（或对称反复比较），我们用 processed 记录已处理的文件名。
+# 最终如果没有任何匹配的文件，
+# 就用 flag printed=0 统一输出 “No files can be replaced…”。
+
+processed
