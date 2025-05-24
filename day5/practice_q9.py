5a6900d09cf642c287b02ee7548a9934a530cefd
JuneY-Y <niyoumengma@gmail.com>
2025-05-24 13:20:03 +1000

    Auto-commit on 2025-05-24 13:20:03

--- day5/practice_q9.py
+++ day5/practice_q9.py
@@ -4,3 +4,12 @@
 	•	优先级：比如先按前 n 个找空格，找不到再全行找
 
 '''
+import sys
+n=int(sys.argv[1])
+filename=sys.argv[2]
+
+with open(filename, 'r') as f:
+    lines=f.readlines()
+
+new_lines=[]
+for line in lines
