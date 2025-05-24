1cebf84c6ac9ce541b9dab730005be939939cee4
JuneY-Y <niyoumengma@gmail.com>
2025-05-24 18:10:05 +1000

    Auto-commit on 2025-05-24 18:10:05

--- day4/practice_q6.py
+++ day4/practice_q6.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python3
 import sys
 
-file1=sys.argv[1]
-file2=sys.argv[2]
+with open(sys.argv[1],'r') as f1:
+    line1=f1.readlines()
+    len1=len(line1)
 
-print(file1)
-lens1=file1.readline()
-print(lens1)
+print(len1)
+with open(sys.argv[2],'r') as f2:
+    line2=f2.readlines()
+    len2=len(line2)
 
-for line in file2:
-    lens2=line.readline()
-print(lens2)
+if (len1!=len2):
+    print(f"Not mirrored: different number of lines: {len1} versus {len2}")
+else:
+    print("Mirrored")
+
+print(len2)
+# print(len1,len2)
