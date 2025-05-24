cc29c3a06e6bf0bb7ccf77e77ad7baff871e1a27
JuneY-Y <niyoumengma@gmail.com>
2025-05-24 21:30:05 +1000

    Auto-commit on 2025-05-24 21:30:05

--- day4/practice_q9.py
+++ day4/practice_q9.py
@@ -1,2 +1,10 @@
 #!/usr/bin/env python3
 ## takes two command line argument, a positive integer n and the name of a file.
+
+import sys
+n=sys.argv[1]
+file=sys.argv[2]
+
+with open (file,'r') as f:
+    lines=[line for line in f]
+
