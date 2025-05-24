4a31db0ca5e5c789bcc187cf667e613fdc35f480
JuneY-Y <niyoumengma@gmail.com>
2025-05-24 18:40:03 +1000

    Auto-commit on 2025-05-24 18:40:03

--- day4/practice_q6.py
+++ day4/practice_q6.py
@@ -1,40 +1,46 @@
 #!/usr/bin/env python3
+#这道题是有坑的，对于具体到哪一行不对应，和条件判断的时候需要输入进list里
+#为什么要list？
+
 import sys
 flag=0
 with open(sys.argv[1],'r') as f1:
     line1=f1.readlines()
     len1=len(line1)
 
-print(len1)
+# print(len1)
 with open(sys.argv[2],'r') as f2:
     line2=f2.readlines()
     len2=len(line2)
-
+line22=list(reversed(line2)) # e.g. the lines can be read into a list.
 if (len1!=len2):
     print(f"Not mirrored: different number of lines: {len1} versus {len2}")
     flag=1
-else:
-    print("Mirrored")
-
-line22=list(reversed(line2)) # e.g. the lines can be read into a list.
-if (flag==1):
-    for i in len1:
-        liness1=lines1.strip()
-        for j in f2:
-            liness2=
-            if (liness1!=liness2):
-                print("Not mirrored: line {i+1} different")
-
-if len(lines1)!=len(lines2):
-    print(f"Not mirrored: different number of lines: {len(lines1)} versus {len(lines2)}")
-elif lines1 == list(reversed(lines2)): # ✔️ 先把 iterator 转换成 list
+elif line1==line22:
     print("Mirrored")
 else:
-    for i in range(len(lines1)):#❌没想道
-        if lines1[i] != lines2[-1-i]:
+    for i in range(len1):# 这里bug了下
+       if line1[i] != line2[-1-i]:#这里卡住了
             print(f"Not mirrored: line {i+1} different")
             sys.exit(0)
+
+# line22=list(reversed(line2)) # e.g. the lines can be read into a list.
+# if (flag==1):
+#     for i in range(len1):
+#        if line1[i] != line2[-1-i]:
+#             print(f"Not mirrored: line {i+1} different")
+#             sys.exit(0)
+
+# if len(lines1)!=len(lines2):
+#     print(f"Not mirrored: different number of lines: {len(lines1)} versus {len(lines2)}")
+# elif lines1 == list(reversed(lines2)): # ✔️ 先把 iterator 转换成 list
+#     print("Mirrored")
+# else:
+#     for i in range(len(lines1)):#❌没想道
+#         if lines1[i] != lines2[-1-i]:
+#             print(f"Not mirrored: line {i+1} different")
+#             sys.exit(0)
             
 
-print(len2)
+# print(len2)
 # print(len1,len2)
