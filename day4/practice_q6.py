fa016ff06bcfb40266da140565a24dcf8df8ecaa
JuneY-Y <niyoumengma@gmail.com>
2025-05-24 19:10:03 +1000

    Auto-commit on 2025-05-24 19:10:03

--- day4/practice_q6.py
+++ day4/practice_q6.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python3
 #这道题是有坑的，对于具体到哪一行不对应，和条件判断的时候需要输入进list里
 #为什么要list？
-
+''''
+时间超时
+reversed 要放在line里面
+i 索引不熟悉
+line1 读取没有去掉strip
+需要记住:line1=[line.rstrip('\n') for line in f1]
+'''
 import sys
 flag=0
 file1=sys.argv[1]
 file2=sys.argv[2]
-with open 
-
+with open (file1,'r') as f1, open (file2,'r') as f2:
+    line1=[line.rstrip('\n') for line in f1]
+    line2=[line.rstrip('\n') for line in f2] # 🌟这里卡了
+len1=len(line1)
+len2=len(line2)
 line22=list(reversed(line2)) # e.g. the lines can be read into a list.
 if (len1!=len2):
     print(f"Not mirrored: different number of lines: {len1} versus {len2}")
@@ -16,7 +25,7 @@ elif line1==line22:
     print("Mirrored")
 else:
     for i in range(len1):# 这里bug了下
-       if line1[i] != line2[-1-i]:#这里卡住了
+       if line1[i] != line2[-1-i]:#这里卡住了🌟
             print(f"Not mirrored: line {i+1} different")
             sys.exit(0)
 
