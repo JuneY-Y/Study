0da8e92c265e2bc7534e3cab57fcbd4b87c731f5
JuneY-Y <niyoumengma@gmail.com>
2025-05-24 14:30:03 +1000

    Auto-commit on 2025-05-24 14:30:03

--- day5/practice_q10.py
+++ day5/practice_q10.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python3
 
 #Counter 和 set 联合使用技巧
+import sys
+from collections import Counter
+
+for line in sys.stdin:
+    words=line.strip().split()
+    balanced_words=[]
+    for word in words:
+        lower_word=word.lower()
+        char_count=Counter(lower_word)
+        freq_set=set(char_count.values())
