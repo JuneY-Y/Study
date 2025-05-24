aeec6662ed8d42099b1d27d52c0cfe7b9a69bc78
JuneY-Y <niyoumengma@gmail.com>
2025-05-24 13:30:03 +1000

    Auto-commit on 2025-05-24 13:30:03

--- day5/practice_q9.py
+++ day5/practice_q9.py
@@ -5,6 +5,7 @@
 
 '''
 import sys
+#which takes two command line argument, n and name of a file
 n=int(sys.argv[1])
 filename=sys.argv[2]
 
@@ -12,4 +13,24 @@ with open(filename, 'r') as f:
     lines=f.readlines()
 
 new_lines=[]
-for line in lines
+for line in lines:
+    line=line.rstrip('\n')
+
+    while len(line)>n:
+        if ' ' not in line:
+            new_lines.append(line)
+            break
+        front=line[:n]
+        if ' ' in front:
+            split_index=front.rfind(' ') #rfind不熟悉
+            new_lines.append(line[:split_index])
+            line=line[split_index+1:]
+        else:
+            split_index=line.find(' ')
+            new_lines.append(line[:split_index])
+            line=line[split_index+1:]
+    if len(line)<=n:
+        new_lines.append(line)
+with open(filename, 'w') as f:
+    for l in new_lines:
+        f.write(l + '\n')
