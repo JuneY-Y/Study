1c31802e8e81ab1ec7c5e71532960b1b6061ab90
JuneY-Y <niyoumengma@gmail.com>
2025-05-24 21:00:02 +1000

    Auto-commit on 2025-05-24 21:00:02

--- day4/practice_q8.sh
+++ day4/practice_q8.sh
@@ -2,13 +2,15 @@
 flag=0
 processed=""
 for file1 in "$@"; do
+        case "$processed" in
+            *" $file1 "*) continue ;;
+        esac
 #commands to replace some of the files with symbolic links.
     for file2 in "$@";do
         if [ "$file1" != "$file2" ]; then
-            if echo "$processed"| grep -qE "(^| )$file1( |$)"; then
-                continue
-            fi
-
+            # if echo "$processed"| grep -qE "(^| )$file1( |$)"; then
+            #     continue
+            # fi
             # continue 
             # 这里🌟 如果processed中已经存在了file2那么跳过还是继续啊continue？
             case "$processed" in
@@ -16,7 +18,7 @@ for file1 in "$@"; do
             esac
             if cmp -s "$file1" "$file2";then
                 echo "ln -s "$file1" "$file2""
-                processed="$processed $file2"
+                processed="$processed $file2 "
                 flag=1
             fi
         fi
@@ -25,7 +27,7 @@ for file1 in "$@"; do
         #     continue
         # fi
     done
-    processed="$processed $file1"
+    processed="$processed $file1 " ##务必拼接的时候加上空格，因为🌟字符串匹配需要明确分隔边界
 done
 # echo "$processed"
 [ $flag -eq 0 ]&& echo "No files can be replaced by symbolic links"  #重新学一遍🌟
