55ef60996f74e585883fb8038ae3872157413b30
JuneY-Y <niyoumengma@gmail.com>
2025-05-24 20:30:02 +1000

    Auto-commit on 2025-05-24 20:30:02

--- day4/practice_q8.sh
+++ day4/practice_q8.sh
@@ -1,16 +1,26 @@
 #!/bin/dash
 flag=0
-procesed=" "
+processed=""
 for file1 in "$@"; do
 #commands to replace some of the files with symbolic links.
     for file2 in "$@";do
         if [ "$file1" != "$file2" ]; then
-            continue
+            # continue 
+            # 这里🌟 如果processed中已经存在了file2那么跳过还是继续啊continue？
+            case "$processed" in
+                *" $file2 "*) continue ;;
+            esac
+            if cmp -s "$file1" "$file2";then
+                echo "ln -s "$file1" "$file2""
+                processed="$processed $file2"
+            fi
         fi
-       if cmp -s "$file1" "$file2";then
-        echo "ln -s "$file1" "$file2""
-        continue
-       fi
+        # if cmp -s "$file1" "$file2";then
+        #     echo "ln -s "$file1" "$file2""
+        #     continue
+        # fi
     done
+    processed="$processed $file2"
     flag=1
 done
+echo "$processed"
