adab51b0d9af122c36b230484a421dd6f7086055
JuneY-Y <niyoumengma@gmail.com>
2025-05-24 19:30:04 +1000

    Auto-commit on 2025-05-24 19:30:04

--- day4/practice_q7.sh
+++ day4/practice_q7.sh
@@ -4,7 +4,23 @@
 #stdout 如何在stderr中print？
 f=$1
 if echo "$f" |grep -Eq "\.";then
-    echo "$f" already has an extension
+    echo ""$f" already has an extension"
     continue;
 fi
 
+if head -n 1 "$f"|grep -qE "^#!";then
+    echo ""$f" does not have a #! line"
+    continue;
+fi
+
+line1=$((head -n 1 "$f"))
+
+if echo "$line1"|grep -qE "python";then
+    ext=".py"
+elif echo "$line1"|grep -qE "sh";then
+    ext=".sh"
+elif echo "$line1"|grep -qE "perl";then
+if 
+
+
+newfile="$f$ext"
