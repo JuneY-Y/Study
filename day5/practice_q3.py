1ad43c49199385eb9da7aa56667b40ede20f3057
JuneY-Y <niyoumengma@gmail.com>
2025-05-24 21:40:03 +1000

    Auto-commit on 2025-05-24 21:40:03

--- day5/practice_q3.py
+++ day5/practice_q3.py
@@ -3,3 +3,11 @@
 '''
 grep -E '\|M$'|cut -d '|' -f3|cut -d "," -f1|tr -d ' '|sort|uniq
 '''
+import sys
+file=sys.argv[1]
+# with open (file,'r') as f: 🌟 in comand   #./practice_q3.py enrollments.txt
+    # lines=f.readlines() ## readlines is a funciton
+
+lines=[line.rstrip("\n") for line in sys.stdin]
+
+print(lines)
