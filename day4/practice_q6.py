91c4cf16321f754004c98acb30c78e8d6c720d58
JuneY-Y <niyoumengma@gmail.com>
2025-05-24 18:20:04 +1000

    Auto-commit on 2025-05-24 18:20:04

--- day4/practice_q6.py
+++ day4/practice_q6.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import sys
-
+flag=0
 with open(sys.argv[1],'r') as f1:
     line1=f1.readlines()
     len1=len(line1)
@@ -12,8 +12,19 @@ with open(sys.argv[2],'r') as f2:
 
 if (len1!=len2):
     print(f"Not mirrored: different number of lines: {len1} versus {len2}")
+    flag=1
 else:
     print("Mirrored")
+
+if (flag==1):
+    for lines1 in f1:
+        liness1=lines1.strip()
+        for lines2 in f2:
+            liness2=reversed(lines2.strip())
+            if (liness1!=liness2)
+
+
+            
 
 print(len2)
 # print(len1,len2)
