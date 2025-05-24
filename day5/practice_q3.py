635eab693710710092b413d4624f496a0c94d7ae
JuneY-Y <niyoumengma@gmail.com>
2025-05-24 21:50:09 +1000

    Auto-commit on 2025-05-24 21:50:09

--- day5/practice_q3.py
+++ day5/practice_q3.py
@@ -4,10 +4,19 @@
 grep -E '\|M$'|cut -d '|' -f3|cut -d "," -f1|tr -d ' '|sort|uniq
 '''
 import sys
-file=sys.argv[1]
+surnames=set()
+
 # with open (file,'r') as f: 🌟 in comand   #./practice_q3.py enrollments.txt
     # lines=f.readlines() ## readlines is a funciton
+for line in sys.stdin:
+    line=line.strip()
+    if line.endswith('\|M'):
+        part=line.strip().split("|")
+        name=part[2]
+        surname=name.split(",")
+        surname=surname[0]
+        surnames.add(surname)
 
-lines=[line.rstrip("\n") for line in sys.stdin]
 
-print(lines)
+
+
