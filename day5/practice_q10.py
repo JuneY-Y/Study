f5e19604180a352239df38e067fccfb16750f289
JuneY-Y <niyoumengma@gmail.com>
2025-05-24 14:40:04 +1000

    Auto-commit on 2025-05-24 14:40:04

--- day5/practice_q10.py
+++ day5/practice_q10.py
@@ -11,3 +11,12 @@ for line in sys.stdin:
         lower_word=word.lower()
         char_count=Counter(lower_word)
         freq_set=set(char_count.values())
+        if len(freq_set)==1:
+            balanced_words.append(word)
+    print(' '.join(balanced_words))
+
+from collections import Counter
+
+# c = Counter('banana')
+# print(c)  # Counter({'a': 3, 'n': 2, 'b': 1})
+# print(c.values())  # dict_values([1, 3, 2])
