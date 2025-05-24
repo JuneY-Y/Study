843f7a5857490b8b258b45027be4a7596202dd57
JuneY-Y <niyoumengma@gmail.com>
2025-05-24 13:50:03 +1000

    Auto-commit on 2025-05-24 13:50:03

--- day5/practice_q9.py
+++ day5/practice_q9.py
@@ -12,16 +12,17 @@ filename=sys.argv[2]
 #sample: script m file.txt 因此需要open file,要全部读取每个line。记录为readlines()
 with open(filename, 'r') as f:
     lines=f.readlines()
-#a maximum desired line length
+#a maximum desired line length,这里为什么用list而不是set呢？
 new_lines=[]
 #for loop 处理line结尾的'\n' 防止分割时候的干扰
 for line in lines:
     line=line.rstrip('\n')
-#
+#当len(line)的长度是>n的时候，就是符合题意的处理
     while len(line)>n:
-        if ' ' not in line:
-            new_lines.append(line)
+        if ' ' not in line: # lines not containing a space character ' ' should not be changed
+            new_lines.append(line) #直接在new_lines里添加整个line即可
             break
+        #需要进行
         front=line[:n]
         if ' ' in front:
             split_index=front.rfind(' ') #rfind不熟悉
