efafecdd44eb2250e4d48e124a525a5bf5d6fa3f
JuneY-Y <niyoumengma@gmail.com>
2025-05-24 20:20:04 +1000

    Auto-commit on 2025-05-24 20:20:04

--- day4/practice_q8.sh
+++ day4/practice_q8.sh
@@ -1,5 +1,16 @@
 #!/bin/dash
-
+flag=0
+procesed=" "
 for file1 in "$@"; do
-
+#commands to replace some of the files with symbolic links.
+    for file2 in "$@";do
+        if [ "$file1" != "$file2" ]; then
+            continue
+        fi
+       if cmp -s "$file1" "$file2";then
+        echo "ln -s "$file1" "$file2""
+        continue
+       fi
+    done
+    flag=1
 done
