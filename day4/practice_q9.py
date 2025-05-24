83f4fe3656ebc3532abbd82fd341dd52e5badd5a
JuneY-Y <niyoumengma@gmail.com>
2025-05-24 23:30:02 +1000

    Auto-commit on 2025-05-24 23:30:01

--- day4/practice_q9.py
+++ day4/practice_q9.py
@@ -2,22 +2,31 @@
 ## takes two command line argument, a positive integer n and the name of a file.
 
 import sys
-n=sys.argv[1]
+n=int(sys.argv[1])
 file=sys.argv[2]
 
 with open (file,'r') as f:
     # lines=[line for line in f]
     for line in f:
-        line=line.strip()
+        line=line.rstrip()
 new_lines=[]
-
-while len(line)>n:
+while len(line) > n:
     if ' ' not in line: #这里用了not in 来进行判断
         new_lines.append(line)
         break
     front=line[:n] #n之前的
     if ' ' in front:
-        split_index=front.rfind(' ')
+        split_index=front.rfind(' ') #找右面的
         new_lines.append(line[:split_index])
         line=line[split_index+1:]
+    else:
+        split_index=line.find(' ')
+        new_lines.append(line[:split_index])
+        line=line[split_index+1:]
+if len(line) <= n:
+    new_lines.append(line)
+
+with open(file, 'w') as f:
+    for l in new_lines:
+        f.write(l+'\n')
 
