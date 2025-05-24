073b4b1d37c23e8bf1b0c831f6bfbf51286d8bfa
JuneY-Y <niyoumengma@gmail.com>
2025-05-24 19:40:03 +1000

    Auto-commit on 2025-05-24 19:40:03

--- day4/practice_q7.sh
+++ day4/practice_q7.sh
@@ -2,25 +2,35 @@
 
 #prints shell commands to add extensions to the filenames.
 #stdout 如何在stderr中print？
-f=$1
-if echo "$f" |grep -Eq "\.";then
-    echo ""$f" already has an extension"
-    continue;
-fi
+for f in "$@";do
+    if echo "$f" |grep -Eq "\.";then
+        echo "# "$f" already has an extension"
+        continue;
+    fi
 
-if head -n 1 "$f"|grep -qE "^#!";then
-    echo ""$f" does not have a #! line"
-    continue;
-fi
+    if ! head -n 1 "$f"|grep -qE "^#!";then
+        echo "# "$f" does not have a #! line"
+        continue;
+    fi
 
-line1=$((head -n 1 "$f"))
+    line1=$(head -n 1 "$f")
 
-if echo "$line1"|grep -qE "python";then
-    ext=".py"
-elif echo "$line1"|grep -qE "sh";then
-    ext=".sh"
-elif echo "$line1"|grep -qE "perl";then
-if 
+   
+
+    if echo "$line1"|grep -qE "python";then
+        ext=".py"
+    elif echo "$line1"|grep -qE "sh";then
+        ext=".sh"
+    elif echo "$line1"|grep -qE "perl";then
+        ext=".pl"
+        continue;
+    else
+        echo "# "$f" no extension for #! line"
+    fi
+
+    newfile="$f$ext"
+    if [ -e ]
 
 
-newfile="$f$ext"
+
+done
