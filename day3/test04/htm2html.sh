d140d3a2435ea7bf7db5a2e0f533e93100005db5
JuneY-Y <niyoumengma@gmail.com>
2025-05-25 18:10:06 +1000

    Auto-commit on 2025-05-25 18:10:06

--- day3/test04/htm2html.sh
+++ day3/test04/htm2html.sh
@@ -1,12 +1,14 @@
 #!/bin/dash
 
-for f in "$@";do
-    original=$(echo "$f"|grep -oE "^[A-Za-z]+\.")
+for f in *.htm;do
+    echo "$f"
+    original=$(echo "$f" | sed "s/\.[A-Za-z]//")
+    echo "$original"
     suffix=".html"
-    original_suffix=$(echo "$f"|grep -oE "\.[A-Za-z]+$")
-    if echo "$original"| grep -qE "$original";then
-        new_suffix=$original$suffix
-        echo "$new_suffix"
-        mv "$f" "$new_suffix"
-    fi  
+    # original_suffix=$(echo "$f"|grep -oE "\.[A-Za-z]+$")
+    # if echo "$original"| grep -qE "$original";then
+    new_suffix="$original$suffix"
+    echo "$new_suffix"
+    mv "$f" "$new_suffix"
+    # fi  
 done
