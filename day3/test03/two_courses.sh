67926f819cdc3fec96544cdb5607ebc72ee1485c
JuneY-Y <niyoumengma@gmail.com>
2025-05-25 17:10:03 +1000

    Auto-commit on 2025-05-25 17:10:03

--- day3/test03/two_courses.sh
+++ day3/test03/two_courses.sh
@@ -1,3 +1,3 @@
 #!/bin/dash
-
-sort -t'|' -k2,2 |uniq -c -f1|grep -E '^ *2'| tr -s ' '|cut -d '|' -f2
+# 这里面sort uniq的考法巧妙的应用了🌟
+sort -t'|' -k2,2|uniq -c -f1|grep -E "^ *2"|cut -d '|' -f2
---
+++ day3/test04/b.h
@@ -0,0 +1,1 @@
+/web/cs2041/25T1/activities/missing_include/files.ln/b.h
