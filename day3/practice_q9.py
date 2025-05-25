680fc2b17eaa6856d872dd3d32ca5375436a93ea
JuneY-Y <niyoumengma@gmail.com>
2025-05-25 15:50:02 +1000

    Auto-commit on 2025-05-25 15:50:02

--- day3/practice_q9.py
+++ day3/practice_q9.py
@@ -1,35 +1,71 @@
 #!/usr/bin/env python3
 
 import sys
-n=int(sys.argv[1])
-file=sys.argv[2]
 
-with open(file, 'r')as f:
-    lines=f.readlines()
+n = int(sys.argv[1])
+file = sys.argv[2]
 
-newline=[]
+with open(file, 'r') as f:
+    lines = f.readlines()
+
+new_lines = []
+
 for line in lines:
-    line=line.rstrip()
-    # newline=[]
-    while len(line)>n: #用while循环进行分块
+    line = line.rstrip()
+    while len(line) > n:
         if ' ' not in line:
-            newline.append(line)
+            new_lines.append(line)
             break
-        front=line[:n] #这个切片不理解
+        front = line[:n]
         if ' ' in front:
-            index=front.rfind(' ')
-            # left=line[:index]
-            # right=line[index+1:]
-            newline.append(line[:index])
-            line=line[index+1:]
+            index = front.rfind(' ')
+            new_lines.append(line[:index])
+            line = line[index + 1:]
         else:
-            index=line.find(' ')
-            newline.append(line[:index])
-            line=line[index+1:]
-        if len(line)<=n:
-            newline.append(line)
+            index = line.find(' ')
+            new_lines.append(line[:index])
+            line = line[index + 1:]
+    if len(line) <= n:
+        new_lines.append(line)
 
 with open(file, 'w') as f:
-    for l in newline:
-        f.write(l+'\n')
+    for l in new_lines:
+        f.write(l + '\n')
+# #!/usr/bin/env python3
+# '''
+# .append() 会直接修改原列表并返回 None!
+# append 不返回新列表，而是就地修改
+# '''
+# import sys
+# n=int(sys.argv[1])
+# file=sys.argv[2]
+
+# with open(file, 'r')as f:
+#     lines=f.readlines()
+
+# newline=[]
+# for line in lines:
+#     line=line.rstrip()
+#     # newline=[]
+#     while len(line)>n: #用while循环进行分块
+#         if ' ' not in line:
+#             newline.append(line)
+#             break
+#         front=line[:n] #这个切片不理解
+#         if ' ' in front:
+#             index=front.rfind(' ')
+#             # left=line[:index]
+#             # right=line[index+1:]
+#             newline.append(line[:index]) 
+#             line=line[index+1:]
+#         else:
+#             index=line.find(' ')
+#             newline.append(line[:index]) 
+#             line=line[index+1:]
+#         if len(line)<=n:
+#             newline.append(line)
+
+# with open(file, 'w') as f:
+#     for l in newline:
+#         f.write(l+'\n')
 
