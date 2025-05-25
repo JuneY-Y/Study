e3bd6c8a536aaa6fbbec2e23c5e97fe2ce4140f9
JuneY-Y <niyoumengma@gmail.com>
2025-05-25 16:30:02 +1000

    Auto-commit on 2025-05-25 16:30:02

--- day3/test03/most_common_first_name.sh
+++ day3/test03/most_common_first_name.sh
@@ -1,1 +1,5 @@
 #!/bin/dash
+
+#If multiple first names are equally most common, 
+#you should print the one that is alphabetically first.
+grep -E "COMP2041|COMP9044" |cut -d "|" -f2,3|sort -n|uniq|cut -d "," -f2|cut -d ' ' -f2|sort|sed "s/ //"|uniq -c|sort -r|sed 's/^ *//'|cut -d ' ' -f2|head -n1
