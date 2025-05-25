0b7190b4ec72716e654d1849be737cef79ac80a8
JuneY-Y <niyoumengma@gmail.com>
2025-05-25 19:30:01 +1000

    Auto-commit on 2025-05-25 19:30:01

--- day3/test04/missing_include.sh
+++ day3/test04/missing_include.sh
@@ -1,1 +1,8 @@
 #!/bin/dash
+file1=$1
+file2=$2
+##need to for loop all of file
+#when file is checked, if include "d.h" not exist d.h in directory. then print it in standout
+for f in *.c;do
+    
+done
