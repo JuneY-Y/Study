f54d2c6952ec419021e25de6a908c0b727b6d7fb
JuneY-Y <niyoumengma@gmail.com>
2025-05-25 16:20:03 +1000

    Auto-commit on 2025-05-25 16:20:03

--- day3/test03/student_first_names.sh
+++ day3/test03/student_first_names.sh
@@ -1,3 +1,3 @@
 #!/bin/dash
 # 完全正确，考试的时候这个题是没有坑的
-cut -d "|" -f2,3|sort -n|uniq|cut -d "," -f2|cut -d ' ' -f2|sort|sed "s/ //"
+grep -E "COMP2041|COMP9044" enrollments.txt|cut -d "|" -f2,3|sort -n|uniq|cut -d "," -f2|cut -d ' ' -f2|sort|sed "s/ //"
