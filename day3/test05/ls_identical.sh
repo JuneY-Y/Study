a047a81614d6d0ce6df8904204bd4c8587bc74e1
JuneY-Y <niyoumengma@gmail.com>
2025-05-25 23:20:04 +1000

    Auto-commit on 2025-05-25 23:20:04

--- day3/test05/ls_identical.sh
+++ day3/test05/ls_identical.sh
@@ -2,10 +2,12 @@
 # ❌ 你在用
 #  if [ “$file1” -eq “$file2” ]
 # 但是 -eq 只能用于数字比较，不能用于字符串比较！
+# 考察cmp -s
+#也可以diff
 
 di1=$1
 di2=$2
-file=""
+
 for file1 in "$di1"/*; do
     filename1=$(basename "$file1")
     # file1=$(echo "$file1"|cut -d '/' -f2)
@@ -17,9 +19,9 @@ for file1 in "$di1"/*; do
         if [ "$filename1" = "$filename2" ];then
             # file=$(echo "$file1"|cut -d '/' -f2)
             if cmp -s "$file1" "$file2";then ##cmp 需要两个文件的完整路径去比较内容。
-                file=$filename1
+                echo "$filename1"
             fi 
         fi
     done
 done
-echo "$file"
+
