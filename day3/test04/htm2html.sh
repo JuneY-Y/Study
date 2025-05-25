7702a383e696ef7073e03d6f1e9031ad0e2af157
JuneY-Y <niyoumengma@gmail.com>
2025-05-25 18:20:06 +1000

    Auto-commit on 2025-05-25 18:20:06

--- day3/test04/htm2html.sh
+++ day3/test04/htm2html.sh
@@ -1,14 +1,20 @@
 #!/bin/dash
 
 for f in *.htm;do
-    echo "$f"
-    original=$(echo "$f" | sed "s/\.[A-Za-z]//")
-    echo "$original"
+    # echo "$f"
+    original=$(echo "$f" | sed "s/\.htm//")
+    # echo "$original"
     suffix=".html"
+    if ! echo "$f"|grep -Eq "\.html";then
     # original_suffix=$(echo "$f"|grep -oE "\.[A-Za-z]+$")
-    # if echo "$original"| grep -qE "$original";then
-    new_suffix="$original$suffix"
-    echo "$new_suffix"
-    mv "$f" "$new_suffix"
+    # if echo "$original"| grep -q E "$original";then
+        new_suffix="$original$suffix"
+        # echo "$new_suffix"
+        mv "$f" "$new_suffix"
+    else
+        echo ""$f" exists" 1>&2
+        exit 1
+    fi
     # fi  
 done
+exit 0
