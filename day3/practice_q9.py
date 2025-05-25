e38dd7245f336b6fb8116539f2639adf73520f99
JuneY-Y <niyoumengma@gmail.com>
2025-05-25 15:10:04 +1000

    Auto-commit on 2025-05-25 15:10:04

--- day3/practice_q9.py
+++ day3/practice_q9.py
@@ -2,4 +2,13 @@
 
 import sys
 n=int(sys.argv[1])
-m=sys.argv[2]
+file=sys.argv[2]
+
+with open(file, 'r')as f:
+    lines=f.readlines()
+
+for line in lines:
+    line=line.rstrip()
+    if len(line)>n:
+
+    else:
