3527182e05ff1770edde3d99279b0b45debbe1cf
JuneY-Y <niyoumengma@gmail.com>
2025-05-25 22:10:02 +1000

    Auto-commit on 2025-05-25 22:10:02

--- day3/test05/ls_identical.sh
+++ day3/test05/ls_identical.sh
@@ -1,8 +1,13 @@
 #!/bin/dash
-for file in "$@":do
-    # while IFS= read -r line; do ## read line by line 
-    count=$(cat "$file"|wc -l)
-    echo "$count"
+## 忘记用-gt了。 而且赋值需要写在全局变量
+maxlines=0
+max_file=""
 
-    
+for file in "$@"; do
+    count=$(wc -l<"$file")
+    if [ "$count" -gt "$max_lines" ]; then
+        max_lines=$count
+        max_file=$file
+    fi
 done
+echo "$max_file"
---
+++ day3/test05/most_lines.sh
@@ -0,0 +1,13 @@
+#!/bin/dash
+## 忘记用-gt了。 而且赋值需要写在全局变量
+maxlines=0
+max_file=""
+
+for file in "$@"; do
+    count=$(wc -l "$file")
+    if [ "$count" -gt "$max_lines" ]; then
+        max_lines=$count
+        max_file=$file
+    fi
+done
+echo "$max_file"
