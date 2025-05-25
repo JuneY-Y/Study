1b3a511fe8fa0744e48775df5583bf970d90daf8
JuneY-Y <niyoumengma@gmail.com>
2025-05-25 15:30:03 +1000

    Auto-commit on 2025-05-25 15:30:03

--- day3/practice_q9.py
+++ day3/practice_q9.py
@@ -7,8 +7,18 @@ file=sys.argv[2]
 with open(file, 'r')as f:
     lines=f.readlines()
 
+newline=[]
 for line in lines:
     line=line.rstrip()
+    # newline=[]
     if len(line)>n:
-
+        if " " not in line:
+            newline.append(line)
+            continue
+        else:
+            index=line.rfind(' ')
+            left=line[:index]
+            right=line[index+1:]
+            newline=newline.append(left)
     else:
+
