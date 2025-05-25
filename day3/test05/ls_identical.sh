e2ef51b4c97d1abd1f3b04cb5026a2a0f06837fc
JuneY-Y <niyoumengma@gmail.com>
2025-05-25 22:40:03 +1000

    Auto-commit on 2025-05-25 22:40:03

--- day3/test05/ls_identical.sh
+++ day3/test05/ls_identical.sh
@@ -1,7 +1,13 @@
 #!/bin/dash
 di1=$1
 di2=$2
-for file in "$1" "$2"; do
-    echo "$file"
-
+file=""
+for file1 in "$di1"/*; do
+    echo "$file1"
+    for file2 in "$di2"/*;do
+        echo "$file2"
+        if [ "$file1" -eq "$file2" ];then
+            file=$(echo "$file1"|)
+        fi
+    done
 done
