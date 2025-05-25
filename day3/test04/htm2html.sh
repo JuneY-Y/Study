12e270a12b489f2b39673cfb0ad54f8f83c05f40
JuneY-Y <niyoumengma@gmail.com>
2025-05-25 18:40:03 +1000

    Auto-commit on 2025-05-25 18:40:03

--- day3/test04/htm2html.sh
+++ day3/test04/htm2html.sh
@@ -1,19 +1,18 @@
 #!/bin/dash
-
+##卡了有一会儿，直接用-e判断是否已经存在即可 ,对应题目是如下：
+#Your script should stop with EXACTLY the error message shown below 
+#and exit status 1 if the .html file already exists.
 for f in *.htm;do
-    echo "$f"
+    # echo "$f"
     original=$(echo "$f" | sed "s/\.htm//")
-    echo "$original"
+    # echo "$original"
     newfile="$original".html
     if [ -e "$newfile" ]; then
-        echo ""$newfile" exists" 1>&2
+        echo "$newfile exists" 1>&2
         exit 1 ## 这里为什么exit1？
     else
         mv "$f" "$newfile"
     fi
-    
-   
-    
    
 done
 exit 0
