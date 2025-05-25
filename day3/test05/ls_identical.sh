2930a090f0eb9e7bf1b77503c74fbe196d418fc5
JuneY-Y <niyoumengma@gmail.com>
2025-05-25 22:50:02 +1000

    Auto-commit on 2025-05-25 22:50:02

--- day3/test05/ls_identical.sh
+++ day3/test05/ls_identical.sh
@@ -3,11 +3,14 @@ di1=$1
 di2=$2
 file=""
 for file1 in "$di1"/*; do
+    
+    file1=$(echo "$file1"|cut -d '/' -f2)
     echo "$file1"
     for file2 in "$di2"/*;do
         echo "$file2"
+        file=$(echo "$file1"|cut -d '/' -f2)
         if [ "$file1" -eq "$file2" ];then
-            file=$(echo "$file1"|)
+            # file=$(echo "$file1"|cut -d '/' -f2)
         fi
     done
 done
