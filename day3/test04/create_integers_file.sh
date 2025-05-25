f25c6c1a3dc0cb1e702139ca35ba2474d6ea2021
JuneY-Y <niyoumengma@gmail.com>
2025-05-25 17:30:03 +1000

    Auto-commit on 2025-05-25 17:30:03

--- day3/test04/create_integers_file.sh
+++ day3/test04/create_integers_file.sh
@@ -1,6 +1,13 @@
 #!/bin/dash
+## 用到了重定向，其实还是需要慢慢调试一下，不过这个原理和思路都记住就好了
 start=$1
 end=$2
 file=$3
 
-seq "$start" "$end" > "$3"
+# seq "$start" "$end" > "$3"
+count=$start
+while [ "$count" -le "$end" ]; do
+    # echo "$count"
+    echo "$count">>"$3"
+    count=$((count+1))
+done
