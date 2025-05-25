bf7d60464221d7046d7aefd53caf57ead466b332
JuneY-Y <niyoumengma@gmail.com>
2025-05-25 20:10:02 +1000

    Auto-commit on 2025-05-25 20:10:02

--- day3/test04/missing_include.sh
+++ day3/test04/missing_include.sh
@@ -1,17 +1,31 @@
 #!/bin/dash
-file1=$1
-file2=$2
-##need to for loop all of file
-#when file is checked, if include "d.h" not exist d.h in directory. then print it in standout
-for f in "$@";do
-    while IFS= read -r line;do
-        check=$(grep -Eo '"[A-Za-z]*.\.h"')
-        
-        check_name=$(echo "$check"|sed 's/"//g')
-        # echo "$check_name"
-        if [ ! -e "$check_name" ];then
-            echo ""$check_name" included into a.c does not exist"
+
+for cfile in "$@"; do
+    while IFS= read -r line; do
+        # 只抓 #include "xxx.h"
+        include=$(echo "$line" | grep -Eo '#include "([^"]+\.h)"' | sed -E 's/#include "//; s/"$//')
+
+        if [ -n "$include" ]; then
+            if [ ! -r "$include" ]; then
+                echo "$include included into $cfile does not exist"
+            fi
         fi
-    done < "$file1"
-    
+    done < "$cfile"
 done
+# #!/bin/dash
+# file1=$1
+# file2=$2
+# ##need to for loop all of file
+# #when file is checked, if include "d.h" not exist d.h in directory. then print it in standout
+# for f in "$@";do
+#     while IFS= read -r line;do
+#         check=$(grep -Eo '"[A-Za-z]*.\.h"')
+        
+#         check_name=$(echo "$check"|sed 's/"//g')
+#         # echo "$check_name"
+#         if [ ! -r "$check_name" ];then
+#             echo ""$check_name" included into a.c does not exist"
+#         fi
+#     done < "$file1"
+    
+# done
