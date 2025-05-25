bea1962ddd8a50e1b4059046bd2d4dd7e3b3b8bc
JuneY-Y <niyoumengma@gmail.com>
2025-05-25 19:40:04 +1000

    Auto-commit on 2025-05-25 19:40:04

--- day3/test04/missing_include.sh
+++ day3/test04/missing_include.sh
@@ -3,6 +3,9 @@ file1=$1
 file2=$2
 ##need to for loop all of file
 #when file is checked, if include "d.h" not exist d.h in directory. then print it in standout
-for f in *.c;do
+for f in "$@";do
+    while IFS= read -r line;do
+        check=$()
+            done < "$file1"
     
 done
