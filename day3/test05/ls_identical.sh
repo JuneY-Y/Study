4a2e69c2f4ac06630859beb41af6b93acbf8dde7
JuneY-Y <niyoumengma@gmail.com>
2025-05-25 22:20:03 +1000

    Auto-commit on 2025-05-25 22:20:03

--- day3/test05/ls_identical.sh
+++ day3/test05/ls_identical.sh
@@ -1,13 +1,6 @@
 #!/bin/dash
-## 忘记用-gt了。 而且赋值需要写在全局变量
-maxlines=0
-max_file=""
 
-for file in "$@"; do
-    count=$(wc -l<"$file")
-    if [ "$count" -gt "$max_lines" ]; then
-        max_lines=$count
-        max_file=$file
-    fi
+for file in ./*; do
+
+
 done
-echo "$max_file"
--- day3/test05/most_lines.sh
+++ day3/test05/most_lines.sh
@@ -1,12 +1,12 @@
 #!/bin/dash
-## 忘记用-gt了。 而且赋值需要写在全局变量
+## 忘记用-gt了。 而且赋值需要写在全局变量 检查变量是否写错
 maxlines=0
 max_file=""
 
 for file in "$@"; do
-    count=$(wc -l "$file")
-    if [ "$count" -gt "$max_lines" ]; then
-        max_lines=$count
+    count=$(wc -l < "$file")
+    if [ "$count" -gt "$maxlines" ]; then
+        maxlines=$count
         max_file=$file
     fi
 done
