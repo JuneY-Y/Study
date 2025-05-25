a7e686794b5650d4cffddbb17605fb9853f3091d
JuneY-Y <niyoumengma@gmail.com>
2025-05-25 13:40:04 +1000

    Auto-commit on 2025-05-25 13:40:04

--- day3/practice_q10.py
+++ day3/practice_q10.py
@@ -3,12 +3,19 @@
 import sys
 from collections import Counter
 
-balance=[]
+
 # file=sys.argv[1]
 for lines in sys.stdin:
     line=lines.strip()
     words=line.split()
+    balance=[] ## 放在这里，可以根据每行line进行处理并且自动换行，当在for line这个forloop外面是只打印一行
     for word in words:
         word_low=word.lower()
         count=Counter(word_low)
+        print(f"the value is :{count.values()}")
+        frequency=set(count.values())  #这个用法还会在哪里被用到，一定要记住了 count.values返回什么
+        print(f"the fre_word:{frequency}")
+        if len(frequency)==1:
+            balance.append(word)
+    print(' '.join(balance))
     
