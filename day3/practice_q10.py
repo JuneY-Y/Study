dd66a75beacb16fd9ae4e9740c2ce7bcc69c8bc6
JuneY-Y <niyoumengma@gmail.com>
2025-05-25 13:30:04 +1000

    Auto-commit on 2025-05-25 13:30:04

--- day3/practice_q10.py
+++ day3/practice_q10.py
@@ -3,4 +3,12 @@
 import sys
 from collections import Counter
 
-balance=set()
+balance=[]
+# file=sys.argv[1]
+for lines in sys.stdin:
+    line=lines.strip()
+    words=line.split()
+    for word in words:
+        word_low=word.lower()
+        count=Counter(word_low)
+    
