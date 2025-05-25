d6e47e542843a5b70969b3cee7153cbeb68424ca
JuneY-Y <niyoumengma@gmail.com>
2025-05-25 20:00:03 +1000

    Auto-commit on 2025-05-25 20:00:03

--- day3/test04/missing_include.sh
+++ day3/test04/missing_include.sh
@@ -5,8 +5,13 @@ file2=$2
 #when file is checked, if include "d.h" not exist d.h in directory. then print it in standout
 for f in "$@";do
     while IFS= read -r line;do
-        check=$(grep -Eo '"*.\.h"')
-        echo "$check"
+        check=$(grep -Eo '"[A-Za-z]*.\.h"')
+        
+        check_name=$(echo "$check"|sed 's/"//g')
+        # echo "$check_name"
+        if [ ! -e "$check_name" ];then
+            echo ""$check_name" included into a.c does not exist"
+        fi
     done < "$file1"
     
 done
