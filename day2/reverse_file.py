49267726881da9a6cda3f266cc849cc9116c9c0f
JuneY-Y <niyoumengma@gmail.com>
2025-05-26 10:50:03 +1000

    Auto-commit on 2025-05-26 10:50:03

--- day2/reverse_file.py
+++ day2/reverse_file.py
@@ -1,3 +1,16 @@
 #!/usr/bin/env python3
 
 import sys
+orfile=sys.argv[1]
+newfile=sys.argv[2]
+
+with open(orfile ,'r') as f1:
+    lines=f1.readlines()
+    # for line in f1: #一行的字符反转 比如第一行42，变为24
+    #     re=list(reversed(line))
+    #     print(re)
+
+
+with open(newfile,'w') as f2:
+     for line in reversed(lines):
+        f2.write(line)
