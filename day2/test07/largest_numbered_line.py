e9ab033593a52bc35fa0c641d261036ad78eb977
JuneY-Y <niyoumengma@gmail.com>
2025-05-26 14:10:03 +1000

    Auto-commit on 2025-05-26 14:10:03

--- day2/test07/largest_numbered_line.py
+++ day2/test07/largest_numbered_line.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
-
+#!/usr/bin/env python3
+import re
 import sys
 save=[]
 for line in sys.stdin:
     for lines in line:
         lines=lines.strip()
-        number=lines.find(r'/d',lines)
+        number=re.fullmatch(r'/d',lines)
         print(number)
+        # for num in range(int(number)):
+        #     print(num)
