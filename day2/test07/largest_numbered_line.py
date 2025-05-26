a4eac443acd22a550a5e53afc393bc38db05fc62
JuneY-Y <niyoumengma@gmail.com>
2025-05-26 14:30:04 +1000

    Auto-commit on 2025-05-26 14:30:04

--- day2/test07/largest_numbered_line.py
+++ day2/test07/largest_numbered_line.py
@@ -2,12 +2,12 @@
 #!/usr/bin/env python3
 import re
 import sys
-
+max_value=0
 for line in sys.stdin:
     # for lines in line:
     lines=line.strip()
     number=re.findall(r'[0-9]+.[0-9]*',lines)
     # print(number)
     # exit(0)
-    for num in range(int(number)):
-        print(num)
+    max_value=max(number)
+print(max_value)
