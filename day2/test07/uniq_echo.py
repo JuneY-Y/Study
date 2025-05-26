50d20c2edf27049c22e7ca4cc62f255a9d3c2e1b
JuneY-Y <niyoumengma@gmail.com>
2025-05-26 12:10:04 +1000

    Auto-commit on 2025-05-26 12:10:04

--- day2/test07/uniq_echo.py
+++ day2/test07/uniq_echo.py
@@ -1,11 +1,23 @@
 #!/usr/bin/env python3
+'''
+1️⃣ 把 set 变成 列表（因为 set 没顺序，最好用 list 保序）
+2️⃣ 用 ' '.join() 拼接成字符串
+改进:
+	•	result.append(arg) → 把第一次出现的参数按顺序存起来
+	•	' '.join(result) → 用空格拼接成一行字符串输出
+	•	最后不用 end=''，因为整个 print 一次性输出好
 
+'''
 import sys
-from collections import Counter
-scan=Counter()
+# from collections import Counter
+seen=set()
+result=[]
 # for i, file in enumerate(sys.argv[+1:]):
 #     print(f"{i} {file}")
-for i in sys.argv[+1:]:
-    scan(i)
-print(set(scan))
-    
+for arg in sys.argv[+1:]:
+    if arg not in seen:
+        # print(arg, end='')  ##很多次都需要end='', 什么时候会用到
+        seen.add(arg)
+        result.append(arg)
+
+print(' '.join(result))
