de50dbc24626198cfe6f0a396a9413fcd5cbd8ef
JuneY-Y <niyoumengma@gmail.com>
2025-05-26 12:40:05 +1000

    Auto-commit on 2025-05-26 12:40:05

--- day2/test07/snap_n.py
+++ day2/test07/snap_n.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+'''
+考察sys.stdin
+'''
+import sys
+result=[]
+n=sys.argv[1]
+for line in sys.stdin:
+    line=line.rstrip('\n')
+    result.append(line)
+    if line in result:
+        print()
