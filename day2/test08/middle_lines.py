d0d902014dcdb275f87147f9c86ee5a13e4a3127
JuneY-Y <niyoumengma@gmail.com>
2025-05-26 18:10:04 +1000

    Auto-commit on 2025-05-26 18:10:04

--- day2/test08/middle_lines.py
+++ day2/test08/middle_lines.py
@@ -2,15 +2,18 @@
 import sys
 
 file=sys.argv[1]
-
+allLine=[]
 with open(file,'r')as f:
     count=0
     for line in f:
         line=line.strip()
+        allLine.append(line)
         count +=1
-        if count%2==0:
-            print("Even")
-        else:
-            print("Odd")
-
+if count%2==0:
+    mid1=line[count//2]
+    mid2=line[count//2 -1]
+    print("Even")
+else:
+    mid1=line[count//2]
+    print("Odd")
 print(count)
