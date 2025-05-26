9f9a296bb0dfb0d9aaf66c8cb8a412b6dfd9eff8
JuneY-Y <niyoumengma@gmail.com>
2025-05-26 17:00:03 +1000

    Auto-commit on 2025-05-26 17:00:03

--- day2/test07/largest_numbered_line.py
+++ day2/test07/largest_numbered_line.py
@@ -1,32 +1,33 @@
-#!/usr/bin/emv python3
+#!/usr/bin/env python3
 
 import sys
 import re
 
-#读入输入行
-lines=sys.stdin.readlines()
-#提取数字
-numbers=[]
+# 读入输入行
+lines = sys.stdin.readlines()
+
+# 提取数字
+numbers = []
 for line in lines:
-    num=re.findall(r'[0-9]+.[0-9]+', line)
-    float_num=list(map(float,num)) #list(map(x , x))
+    num = re.findall(r'[+-]?(?:[0-9]+\.?[0-9]*|\.[0-9]+)', line)
+    float_num = list(map(float, num))
     numbers.append(float_num)
-#每行最大值
-Max=[]
-for list in numbers:
+
+# 每行最大值
+Max = []
+for num_list in numbers:
     try:
-        max=max(list)
-        Max.append(max)
+        line_max = max(num_list)
+        Max.append(line_max)
     except ValueError:
-        pass  ##这一行没有数字，那么跳过
+        pass  # 这一行没有数字，跳过
 
-#所有行中最大值
+# 所有行中最大值
 try:
-    all_max=max(Max)
-    #这个最大值所在的行
-    for idx,list in enumerate(numbers):
-        if all_max in list:
-            print(lines[idx],end='')
-
+    all_max = max(Max) #不要起冲突的变量名
+    # 打印包含最大值的行
+    for idx, num_list in enumerate(numbers):
+        if all_max in num_list:
+            print(lines[idx], end='')
 except ValueError:
     pass
