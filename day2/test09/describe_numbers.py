b0192e2def4a338a573c07b9fd95592e989b658d
JuneY-Y <niyoumengma@gmail.com>
2025-05-26 23:00:04 +1000

    Auto-commit on 2025-05-26 23:00:04

--- day2/test09/describe_numbers.py
+++ day2/test09/describe_numbers.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 '''
 The count of how many numbers
-The count of how many unique numbers
+The count of how many unique numbers 不同的数字
 The minimum number
 The maximum number
 The mean of the numbers
 The median of the numbers
 The mode of the numbers = import mul
 The sum of the numbers
-The product of the numbers
+The product of the numbers 数字的乘积
 
 	•	len()、set()、min()、max()、sum()
 	•	reduce()、mul()（算乘积）
@@ -17,8 +17,8 @@ The product of the numbers
 import sys
 from statistics import mean, median,mode
 from functools import reduce
-import operator
-#from operator import mul
+# import operator
+from operator import mul
 
 
 numbers=list(map(int,sys.argv[1:])) # 转为整数列表
@@ -30,4 +30,11 @@ count=len(numbers)
 unique_count=len(set(numbers))
 total=sum(numbers)
 product=reduce(mul, numbers, 1)
-mini=min(number)
+mini=min(numbers)
+maxi=max(numbers)
+avg=mean(numbers)  #mean → 平均数（算术平均值
+med=median(numbers)
+try:
+    mod=mode(numbers) ##uniq number
+except:
+    mod="no unique mode"
