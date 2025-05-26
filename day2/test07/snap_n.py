50bd62f72b513eeb92b29133e5594f64c85a2732
JuneY-Y <niyoumengma@gmail.com>
2025-05-26 13:00:05 +1000

    Auto-commit on 2025-05-26 13:00:04

--- day2/test07/snap_n.py
+++ day2/test07/snap_n.py
@@ -1,15 +1,26 @@
 #!/usr/bin/env python3
 '''
 考察sys.stdin
+再写一遍
 '''
 import sys
-result=[]
-n=sys.argv[1]
+
+n = int(sys.argv[1])
+counts = {}
+
 for line in sys.stdin:
-    line=line.rstrip('\n')
-    result.append(line)
-    if line in result:
-        for i in range(n):
+    line = line.rstrip('\n')
+    counts[line] = counts.get(line, 0) + 1  #	counts.get(line, 0) + 1 → 如果没见过，就是 1；见过就 +1
+    if counts[line] == n:
+        print(f"Snap: {line}")
+        break
+# result=[]
+# n=sys.argv[1]
+# for line in sys.stdin:
+#     line=line.rstrip('\n')
+#     result.append(line)
+#     if line in result:
+#         for i in range(n):
             
-    else
-        continue
+#     else
+#         continue
