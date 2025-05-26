8b2eabe55824ec94721cfb9117d7e0d383830102
JuneY-Y <niyoumengma@gmail.com>
2025-05-26 22:10:05 +1000

    Auto-commit on 2025-05-26 22:10:05

---
+++ day2/test09/describe_numbers.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+'''
+The count of how many numbers
+The count of how many unique numbers
+The minimum number
+The maximum number
+The mean of the numbers
+The median of the numbers
+The mode of the numbers
+The sum of the numbers
+The product of the numbers
+
+	•	len()、set()、min()、max()、sum()
+	•	reduce()、mul()（算乘积）
+	•	statistics.mean、median、mode(统计值)
+'''
+import sys
+
