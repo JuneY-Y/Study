9c16cf2104b03a2c7d5110063fdaf88a4520cf48
JuneY-Y <niyoumengma@gmail.com>
2025-05-26 10:20:04 +1000

    Auto-commit on 2025-05-26 10:20:04

--- day2/create_integers_file.py
+++ day2/create_integers_file.py
@@ -1,4 +1,4 @@
-#!/usr/bim/env python3
+#!/usr/bin/env python3
 '''
 实现和week test4.1 相同的script function,用py实现
 '''
@@ -8,8 +8,14 @@ end=int(sys.argv[2])
 file=sys.argv[3]
 count=start
 list=[]
-with open (file, 'w') as f:
-    if (count<end):
+while (count<end+1):
         list.append(count)
         count=count+1
-        
+       
+with open (file, 'w') as f:
+        for line in list:
+            f.write(str(line)+'\n')
+
+# with open(file, 'w') as f:
+#     for i in range(start, end):
+#         f.write(str(i) + '\n')
