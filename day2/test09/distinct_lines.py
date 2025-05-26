7336dc565b353148ace315c1f24ea197eae1f1b3
JuneY-Y <niyoumengma@gmail.com>
2025-05-26 23:30:04 +1000

    Auto-commit on 2025-05-26 23:30:04

--- day2/test09/distinct_lines.py
+++ day2/test09/distinct_lines.py
@@ -2,10 +2,16 @@
 
 import sys
 from statistics import mode
-N=sys.argv[1]
+N=int(sys.argv[1])
 list=[]
+count=0
 for lines in sys.stdin:
-    
+    lines=lines.strip()
+    list.append(lines)
+    count=count+1
+    if N==list[N-1]:
+        print(f"{N} distinct lines seen after {count} lines read.")
+        exit(0)
 
 
 
