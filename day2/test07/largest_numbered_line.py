44c91ea22e0f8030574d46aa2c7853541cef4969
JuneY-Y <niyoumengma@gmail.com>
2025-05-26 16:50:04 +1000

    Auto-commit on 2025-05-26 16:50:04

--- day2/test07/largest_numbered_line.py
+++ day2/test07/largest_numbered_line.py
@@ -11,3 +11,22 @@ for line in lines:
     num=re.findall(r'[0-9]+.[0-9]+', line)
     float_num=list(map(float,num)) #list(map(x , x))
     numbers.append(float_num)
+#每行最大值
+Max=[]
+for list in numbers:
+    try:
+        max=max(list)
+        Max.append(max)
+    except ValueError:
+        pass  ##这一行没有数字，那么跳过
+
+#所有行中最大值
+try:
+    all_max=max(Max)
+    #这个最大值所在的行
+    for idx,list in enumerate(numbers):
+        if all_max in list:
+            print(lines[idx],end='')
+
+except ValueError:
+    pass
