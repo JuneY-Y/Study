9206b644af52317bb6daa4df132939dd6adf3dfe
JuneY-Y <niyoumengma@gmail.com>
2025-05-26 11:10:02 +1000

    Auto-commit on 2025-05-26 11:10:02

--- day2/nth_line.py
+++ day2/nth_line.py
@@ -1,2 +1,12 @@
 #!/usr/bin/env python3
 
+import sys
+n=int(sys.argv[1])
+file=sys.argv[2]
+
+with open (file, 'r') as f:
+    for line in f:
+        for i in line:
+            print(line)
+    # for n in range(lines):
+    #     print()
