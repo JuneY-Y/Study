34b7a8879f705dadd95b201bd743cc3f31b3a4d2
JuneY-Y <niyoumengma@gmail.com>
2025-05-26 20:30:04 +1000

    Auto-commit on 2025-05-26 20:30:04

--- day2/test08/sort_file_lines.py
+++ day2/test08/sort_file_lines.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 import sys
 file=sys.argv[1]
+compare=[]
 with open (file, 'r') as f:
     line=f.readlines() #readlines() 会自动把每一行，包括结尾 \n，作为独立字符串读进列表
+    for lines in line:
+        compare.append(len(lines))
+
+print(compare)
