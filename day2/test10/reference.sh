80a75b7800bf5a852bbb57e61d9522adc2d8721d
JuneY-Y <niyoumengma@gmail.com>
2025-05-27 11:50:05 +1000

    Auto-commit on 2025-05-27 11:50:05

--- day2/test10/reference.sh
+++ day2/test10/reference.sh
@@ -1,6 +1,20 @@
 #!/bin/dash
 
-lines=()
-while read -r lines;do
-    lines+=("$line")
-done
+# 保存 stdin 到临时文件
+tmpfile=$(mktemp)
+cat > "$tmpfile"
+
+# 按行处理
+lineno=0
+while read -r line; do
+    lineno=$((lineno + 1))
+    if echo "$line" | grep -Eq '^#[0-9]+$'; then
+        n=$(echo "$line" | sed 's/#//')
+        sed -n "${n}p" "$tmpfile"
+    else
+        echo "$line"
+    fi
+done < "$tmpfile"
+
+# 清理临时文件
+rm "$tmpfile"
