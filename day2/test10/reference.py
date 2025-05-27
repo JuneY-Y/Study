f0610bf2dc89b7a6a16aebb6d59b8a0f44a7d6a2
JuneY-Y <niyoumengma@gmail.com>
2025-05-27 13:00:04 +1000

    Auto-commit on 2025-05-27 13:00:04

--- day2/test10/reference.py
+++ day2/test10/reference.py
@@ -9,7 +9,7 @@
 	•	for line in sys.stdin: → 直接按行读入。
 	•	rstrip('\n') → 只去掉换行，不误删行内空格。
 	•	print(line) → 默认会自动换行，所以不需要 end=''。
-	•	re.search(r'#\d+', line) → 检查是否包含 # 后跟数字（如 #7）。
+	•	re.search(r'#\d+', line) → 检查是否包含 # 后跟数字（如 #7)。
 '''
 #Lines of the form #n (where n is an integer value), should be replaced by the n’th line of input.
 # 理解不了就直接带n进行赋值，即可
@@ -22,7 +22,7 @@ lines=sys.stdin.readlines()
 for line in lines: 
     if line[-1] == '\n':
         line=line[:-1]
-    #用正则匹配
+    #用正则匹
     m=re.fullmatch(r'#(\d+)', line)
     if m:
         # print(f"this is what I want to see{m.group(1)}")
