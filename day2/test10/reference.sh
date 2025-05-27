95f2334404b34e04461bea7522346d45579f31f9
JuneY-Y <niyoumengma@gmail.com>
2025-05-27 12:00:04 +1000

    Auto-commit on 2025-05-27 12:00:04

--- day2/test10/reference.sh
+++ day2/test10/reference.sh
@@ -1,16 +1,16 @@
 #!/bin/dash
-
+#1️⃣ 直接一次性读两遍 stdin
 # 保存 stdin 到临时文件
 tmpfile=$(mktemp)
 cat > "$tmpfile"
 
 # 按行处理
-lineno=0
+count=0
 while read -r line; do
-    lineno=$((lineno + 1))
+    count=$((lineno + 1))
     if echo "$line" | grep -Eq '^#[0-9]+$'; then
         n=$(echo "$line" | sed 's/#//')
-        sed -n "${n}p" "$tmpfile"
+        sed -n "${n}p" "$tmpfile" #sed -n '3p' file 打印文件第3行
     else
         echo "$line"
     fi
