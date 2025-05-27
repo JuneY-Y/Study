62f1673060254633e14aa1d60409da6d653ccd64
JuneY-Y <niyoumengma@gmail.com>
2025-05-27 13:30:01 +1000

    Auto-commit on 2025-05-27 13:30:01

--- day1_s/map.py
+++ day1_s/map.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
+'''
+map(func, iterable)
+→ 把 func 应用到 iterable 里的每个元素上，返回一个可迭代对象。
+'''
+#iterable
 nums = [1, 2, 3]
 squared = map(lambda x: x * x, nums)
 print(list(squared))  # [1, 4, 9]
