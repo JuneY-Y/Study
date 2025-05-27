0510d385f813f8dab0e16f4ccced1bca90dc3893
JuneY-Y <niyoumengma@gmail.com>
2025-05-27 12:30:06 +1000

    Auto-commit on 2025-05-27 12:30:06

--- day2/test10/text_round.py
+++ day2/test10/text_round.py
@@ -2,9 +2,16 @@
 import sys
 import re
 #handle read all lines
-# lines=sys.stdin.readlines()
-file=sys.argv[1]
-with open (file,'r')as f:
-    for line in f:
-        if re.findall(r'^[0-9]+/.[0-9]+',line):
-            print(line)
+lines=sys.stdin.readlines()
+for line in lines:
+    if m:=re.findall(r'[+-]?(?:[0-9]+\.?[0-9]*|\.[0-9]+)',line):
+        print(m)
+        if m is float:
+            if y.x>5:
+                y=y+1
+            else:
+                y=y-1
+            
+                
+
+
