888f5291f428a0e547e3f2bcdc24966ab34a900b
JuneY-Y <niyoumengma@gmail.com>
2025-05-27 12:20:03 +1000

    Auto-commit on 2025-05-27 12:20:03

--- day2/test10/text_round.py
+++ day2/test10/text_round.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3
+import sys
+import re
+#handle read all lines
+# lines=sys.stdin.readlines()
+file=sys.argv[1]
+with open (file,'r')as f:
+    for line in f:
+        if re.findall(r'^[0-9]+/.[0-9]+',line):
+            print(line)
