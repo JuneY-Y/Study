be97dcbda9f3376af83eb244748a4d998c04bed7
JuneY-Y <niyoumengma@gmail.com>
2025-07-22 10:00:02 +1000

    Auto-commit on 2025-07-22 10:00:02

--- G_Jul22/leetcode1.py
+++ G_Jul22/leetcode1.py
@@ -6,3 +6,14 @@ class Solution(object):
 
         def dfs(i):
             for j in range(n):
+                if isConnected[i][j] == 1 and j not in visited:
+                    visited.add(j)
+                    dfs(j)
+
+        count=0
+        for i in range(n):
+            if i not in visited:
+                visited.add(i)
+                dfs(i)
+                count += 1
+        return count
